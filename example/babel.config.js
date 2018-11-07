1878d833a869f8a70016e95704056f23db0576e1
Donald Hruska <donaldhruska@gmail.com>
2018-11-07 15:43:48 -0600

    Get example app working with local components (#77)
    * Get example app working with local components
    
    * Add portal
    
    * App.js update

--- example/babel.config.js
+++ example/babel.config.js
@@ -1,6 +1,18 @@
 module.exports = function(api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
+    presets: ["babel-preset-expo"],
+    plugins: [
+      [
+        "module-resolver",
+        {
+          alias: {
+            "@draftbit/ui": "../src/index",
+            "@draftbit/ui/types": "../types",
+            "react-native-vector-icons": "@expo/vector-icons"
+          }
+        }
+      ]
+    ]
   };
 };

87bff24c256d3f113cbbc3256b2292b5b51d8e3c
Donald Hruska <donaldhruska@gmail.com>
2018-11-08 14:47:41 -0600

    @dhruska/divider (#79)
    * Get example app working with local components
    
    * Add portal
    
    * Add Divider
    
    * Remove color library
    
    * Remove rest

--- src/index.js
+++ src/index.js
@@ -4,3 +4,5 @@ export { withTheme, ThemeProvider } from "./core/theming";
 
 export { default as Provider } from "./core/Provider";
 export { default as DefaultTheme } from "./styles/DefaultTheme";
+
+export { default as Divider } from "./components/Divider";

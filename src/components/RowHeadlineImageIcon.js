d7f26530022e4de429e67539f28bbe23ddffffde
Donald Hruska <donaldhruska@gmail.com>
2018-11-14 17:06:16 -0600

    @dhruska/rowheadlineimageicon (#103)
    * Add seed data
    
    * import
    
    * Capitalization

--- src/components/RowHeadlineImageIcon.js
+++ src/components/RowHeadlineImageIcon.js
@@ -3,6 +3,7 @@
 import * as React from "react";
 import { View, Image } from "react-native";
 import { withTheme } from "../core/theming";
+import { COMPONENT_TYPES, FORM_TYPES } from "../core/component-types";
 import type { Theme } from "../types";
 import Row from "./Row";
 import Icon from "./Icon";
@@ -11,7 +12,7 @@ import Config from "./Config";
 type Props = {
   title: string,
   subtitle?: string,
-  multilineSubtitle?: string,
+  multilineSubtitle?: boolean,
   image: string | { uri: string },
   icon: string,
   style?: any,
@@ -63,3 +64,247 @@ class RowHeadlineImageIcon extends React.Component<Props> {
 }
 
 export default withTheme(RowHeadlineImageIcon);
+
+export const SEED_DATA = [
+  {
+    name: "Row Single Line Headline Icon",
+    tag: "RowHeadlineImageIcon",
+    description:
+      "A row with left aligned headline text and a right aligned icon",
+    category: COMPONENT_TYPES.row,
+    preview_image_url:
+      "https://res.cloudinary.com/altos/image/upload/v1541096678/draftbit/library/jigsaw-1.0/reps/Row_SingleLine_HeadlineIcon.png",
+    supports_list_render: true,
+    props: {
+      title: {
+        label: "Title",
+        description: "Headline text to display",
+        type: FORM_TYPES.string,
+        value: "Beautiful West Coast Villa",
+        editable: true
+      },
+      icon: {
+        label: "Icon",
+        description: "Icon to display",
+        type: FORM_TYPES.icon,
+        value: "favorite",
+        editable: true
+      }
+    },
+    layout: {
+      width: 343,
+      height: 57
+    }
+  },
+  {
+    name: "Row Single Line Headline Icon Image",
+    tag: "RowHeadlineImageIcon",
+    description:
+      "A row with left aligned image and headline text and a right aligned icon",
+    category: COMPONENT_TYPES.row,
+    preview_image_url:
+      "https://res.cloudinary.com/altos/image/upload/v1541096668/draftbit/library/jigsaw-1.0/reps/Row_SingleLine_HeadlineIconImage.png",
+    supports_list_render: true,
+    props: {
+      title: {
+        label: "Title",
+        description: "Headline text to display",
+        type: FORM_TYPES.string,
+        value: "Beautiful West Coast Villa",
+        editable: true
+      },
+      icon: {
+        label: "Icon",
+        description: "Icon to display",
+        type: FORM_TYPES.icon,
+        value: "favorite",
+        editable: true
+      },
+      image: {
+        label: "Image",
+        description: "Image to display",
+        type: FORM_TYPES.remoteImage,
+        value: null,
+        editable: true
+      }
+    },
+    layout: {
+      width: 343,
+      height: 57
+    }
+  },
+  {
+    name: "Row Double Line Headline Icon",
+    tag: "RowHeadlineImageIcon",
+    description:
+      "A row with left aligned headline text and subtitle text and a right aligned icon",
+    category: COMPONENT_TYPES.row,
+    preview_image_url:
+      "https://res.cloudinary.com/altos/image/upload/v1541096695/draftbit/library/jigsaw-1.0/reps/Row_DoubleLine_HeadlineIcon.png",
+    supports_list_render: true,
+    props: {
+      title: {
+        label: "Title",
+        description: "Headline text to display",
+        type: FORM_TYPES.string,
+        value: "Beautiful West Coast Villa",
+        editable: true
+      },
+      subtitle: {
+        label: "Subtitle",
+        description: "Subtitle text to display",
+        type: FORM_TYPES.string,
+        value: "San Diego",
+        editable: true
+      },
+      icon: {
+        label: "Icon",
+        description: "Icon to display",
+        type: FORM_TYPES.icon,
+        value: "favorite",
+        editable: true
+      }
+    },
+    layout: {
+      width: 343,
+      height: 83
+    }
+  },
+  {
+    name: "Row Double Line Headline Image Icon",
+    tag: "RowHeadlineImageIcon",
+    description:
+      "A row with left aligned headline text and subtitle text and a right aligned icon",
+    category: COMPONENT_TYPES.row,
+    preview_image_url:
+      "https://res.cloudinary.com/altos/image/upload/v1541096706/draftbit/library/jigsaw-1.0/reps/Row_DoubleLine_HeadlineImageIcon.png",
+    supports_list_render: true,
+    props: {
+      title: {
+        label: "Title",
+        description: "Headline text to display",
+        type: FORM_TYPES.string,
+        value: "Beautiful West Coast Villa",
+        editable: true
+      },
+      subtitle: {
+        label: "Subtitle",
+        description: "Subtitle text to display",
+        type: FORM_TYPES.string,
+        value: "San Diego",
+        editable: true
+      },
+      icon: {
+        label: "Icon",
+        description: "Icon to display",
+        type: FORM_TYPES.icon,
+        value: "favorite",
+        editable: true
+      },
+      image: {
+        label: "Image",
+        description: "Image to display",
+        type: FORM_TYPES.remoteImage,
+        value: null,
+        editable: true
+      }
+    },
+    layout: {
+      width: 375,
+      height: 83
+    }
+  },
+  {
+    name: "Row Multiline Headline Icon",
+    tag: "RowHeadlineImageIcon",
+    description:
+      "A row with left aligned headline text and multiline subtitle text and a right aligned icon",
+    category: COMPONENT_TYPES.row,
+    preview_image_url:
+      "https://res.cloudinary.com/altos/image/upload/v1541096681/draftbit/library/jigsaw-1.0/reps/Row_Multiline_HeadlineIcon.png",
+    supports_list_render: true,
+    props: {
+      title: {
+        label: "Title",
+        description: "Headline text to display",
+        type: FORM_TYPES.string,
+        value: "Beautiful West Coast Villa",
+        editable: true
+      },
+      subtitle: {
+        label: "Subtitle",
+        description: "Subtitle text to display",
+        type: FORM_TYPES.string,
+        value:
+          "San Diego is a city on the Pacific coast of California known for its beaches, parks and warm climate",
+        editable: true
+      },
+      icon: {
+        label: "Icon",
+        description: "Icon to display",
+        type: FORM_TYPES.icon,
+        value: "favorite",
+        editable: true
+      },
+      multilineSubtitle: {
+        type: FORM_TYPES.boolean,
+        value: true,
+        editable: false
+      }
+    },
+    layout: {
+      width: 375,
+      height: 105
+    }
+  },
+  {
+    name: "Row Multiline Headline Image Icon",
+    tag: "RowHeadlineImageIcon",
+    description:
+      "A row with left aligned image, headline text, and multiline subtitle text, and a right aligned icon",
+    category: COMPONENT_TYPES.row,
+    preview_image_url:
+      "https://res.cloudinary.com/altos/image/upload/v1541096707/draftbit/library/jigsaw-1.0/reps/Row_Multiline_HeadlineImageIcon.png",
+    supports_list_render: true,
+    props: {
+      title: {
+        label: "Title",
+        description: "Headline text to display",
+        type: FORM_TYPES.string,
+        value: "Beautiful West Coast Villa",
+        editable: true
+      },
+      subtitle: {
+        label: "Subtitle",
+        description: "Subtitle text to display",
+        type: FORM_TYPES.string,
+        value:
+          "San Diego is a city on the Pacific coast of California known for its beaches, parks and warm climate",
+        editable: true
+      },
+      icon: {
+        label: "Icon",
+        description: "Icon to display",
+        type: FORM_TYPES.icon,
+        value: "favorite",
+        editable: true
+      },
+      image: {
+        label: "Image",
+        description: "Image to display",
+        type: FORM_TYPES.remoteImage,
+        value: null,
+        editable: true
+      },
+      multilineSubtitle: {
+        type: FORM_TYPES.boolean,
+        value: true,
+        editable: false
+      }
+    },
+    layout: {
+      width: 375,
+      height: 105
+    }
+  }
+];

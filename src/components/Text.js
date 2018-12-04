860156eb5207cfd5ceb42ad1a2d18509f185c26c
Donald Hruska <donaldhruska@gmail.com>
2018-12-04 15:24:52 -0600

    Add image and text

--- src/components/Text.js
+++ src/components/Text.js
@@ -3,6 +3,7 @@
 import * as React from "react";
 import { Text as NativeText, I18nManager } from "react-native";
 import { withTheme } from "../core/theming";
+import { COMPONENT_TYPES, FORM_TYPES } from "../core/component-types";
 import type { Theme } from "../types";
 
 type Props = React.ElementConfig<typeof NativeText> & {
@@ -50,4 +51,74 @@ class Text extends React.Component<Props> {
   }
 }
 
-export default withTheme(Text);
+export const SEED_DATA = {
+  name: "Text",
+  tag: "Text",
+  description: "A basic Text component",
+  type: COMPONENT_TYPES.primitive,
+  supports_list_render: false,
+  preview_image_url:
+    "https://res.cloudinary.com/altos/image/upload/v1543956594/draftbit/Jigsaw/Text.svg",
+  layout: {
+    width: 300,
+    height: 20
+  },
+  props: {
+    children: {
+      label: "Text",
+      description: "Text for the text",
+      editable: true,
+      required: true,
+      type: FORM_TYPES.string,
+      value: "Hello World!"
+    },
+    style: {
+      label: "Style",
+      description: "Text Style",
+      editable: true,
+      required: false,
+      type: FORM_TYPES.typeStyle,
+      value: null
+    },
+    textAlign: {
+      label: "Text Align",
+      description: "Text Align",
+      editable: true,
+      required: false,
+      type: FORM_TYPES.flatArray,
+      options: ["Auto", "Left", "Right", "Center", "Justify"]
+    },
+    textDecorationLine: {
+      label: "Text Decoration",
+      description: "Text Decoration",
+      editable: true,
+      required: false,
+      type: FORM_TYPES.flatArray,
+      options: ["None", "Underline", "Line-Through", "Underline Line-Through"]
+    },
+    textDecorationColor: {
+      label: "Text Decoration Color",
+      description: "Text Decoration Color",
+      editable: true,
+      required: false,
+      type: FORM_TYPES.color,
+      value: "#000000"
+    },
+    textDecorationStyle: {
+      label: "Text Decoration Style",
+      description: "Text Decoration Style",
+      editable: true,
+      required: false,
+      type: FORM_TYPES.flatArray,
+      options: ["None", "Solid", "Double", "Dotted", "Dashed"]
+    },
+    color: {
+      label: "Text Color",
+      description: "Text Color",
+      editable: true,
+      required: false,
+      type: FORM_TYPES.color,
+      value: "#000000"
+    }
+  }
+};

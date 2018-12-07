9d2bc6dddc812d84a317883417ce50a31cb26ceb
Donald Hruska <donaldhruska@gmail.com>
2018-12-07 12:54:01 -0600

    Fix component categorization

--- src/components/AvatarEdit.js
+++ src/components/AvatarEdit.js
@@ -80,6 +80,8 @@ export const SEED_DATA = {
   tag: "AvatarEdit",
   description: "An avatar with an edit icon in the top right",
   category: COMPONENT_TYPES.button,
+  preview_image_url:
+    "https://res.cloudinary.com/altos/image/upload/v1538751224/draftbit/Jigsaw/AvatarEdit.png",
   props: {
     size: {
       label: "Size",
@@ -103,7 +105,7 @@ export const SEED_DATA = {
     }
   },
   layout: {
-    width: 134,
-    height: 134
+    width: 64,
+    height: 64
   }
 };

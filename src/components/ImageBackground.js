17b4515312acdff8f97967ef4bccb7b002485070
Donald Hruska <donaldhruska@gmail.com>
2019-04-03 10:07:07 -0500

    Image background remove absolute position

--- src/components/ImageBackground.js
+++ src/components/ImageBackground.js
@@ -15,11 +15,8 @@ export const SEED_DATA = {
   type: COMPONENT_TYPES.primitive,
   supports_list_render: false,
   layout: {
-    position: "absolute",
-    top: 0,
-    bottom: 0,
-    left: 0,
-    right: 0
+    width: "100%",
+    height: "100%"
   },
   props: {
     source: {

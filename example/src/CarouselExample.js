edb595daf32ecfac130c1d12dba1643e91509814
Donald Hruska <donaldhruska@gmail.com>
2019-05-01 10:33:46 -0500

    Update images and dot color in example

--- example/src/CarouselExample.js
+++ example/src/CarouselExample.js
@@ -9,6 +9,13 @@ type Props = {
   theme: Theme
 };
 
+const IMAGES = [
+  "https://apps-draftbit-com.s3.amazonaws.com/xxQUEDSJ/assets/b23d4319-96fc-4ab5-813a-4538a84a6fd6",
+  "https://apps-draftbit-com.s3.amazonaws.com/xxQUEDSJ/assets/9eb80357-b96b-4c49-be91-7e46629a0955",
+  "https://apps-draftbit-com.s3.amazonaws.com/xxQUEDSJ/assets/be668ad0-f62d-4b43-8261-023c9406b27d",
+  "https://apps-draftbit-com.s3.amazonaws.com/xxQUEDSJ/assets/96c42572-3a34-47a5-95b0-0c332f9505ef"
+];
+
 class CarouselExample extends React.Component<Props> {
   render() {
     const {
@@ -21,7 +28,7 @@ class CarouselExample extends React.Component<Props> {
     return (
       <View style={[styles.container, { backgroundColor: background }]}>
         <Text style={{ padding: spacing.large }}>Carousel</Text>
-        <Carousel />
+        <Carousel images={IMAGES} dotColor={background} />
       </View>
     );
   }

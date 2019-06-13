b7ea06d45d51a082822fb2fa02301e7e16c5c564
Sebastian Kurpiel <sebbykurp@gmail.com>
2019-06-13 13:44:44 -0500

    Removed aspectRatio

--- src/components/ProgressIndicator.web.js
+++ src/components/ProgressIndicator.web.js
@@ -1,6 +1,6 @@
 import React from "react"
 import UnsupportedView from "./UnsupportedView"
 
-export default function ProgressIndicator({ aspectRatio, style }) {
-  return <UnsupportedView tag="ProgressIndicator" style={[style, { aspectRatio }]} />
+export default function ProgressIndicator({ style }) {
+  return <UnsupportedView tag="ProgressIndicator" style={[style]} />
 }

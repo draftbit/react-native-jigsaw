e8c7b6a863cf599b5419dd4aa50bcf0c1d33b138
Sebastian Kurpiel <sebbykurp@gmail.com>
2020-01-17 16:46:29 -0600

    Set an empty array if "options" prop is not passed in. (#290)
    * Set an empty array if options is not passed in
    
    * implemented a defaultProp instead of the sorta hacky way I did before

--- src/components/RadioButtonGroup.js
+++ src/components/RadioButtonGroup.js
@@ -24,6 +24,7 @@ type Props = {
 }
 
 class RadioButtonGroup extends React.Component<Props> {
+  static defaultProps = { options: [] }
   state = { selected: this.props.defaultSelection }
 
   onPress = selected => {

cc9b0c089a3e62db3c8ff8607bd6a1818f5dc3a5
Peter Piekarczyk <peterpiekarczyk@gmail.com>
2020-02-04 11:26:18 -0600

    Revert "remove all web stuff"
    This reverts commit e43704879eeaf3ad5ca79047b61a0566c19209e2.

---
+++ src/components/ProgressBar.web.js
@@ -0,0 +1,18 @@
+import * as React from "react"
+import { View } from "react-native"
+
+export default function ProgressBar({
+  progress,
+  borderRadius,
+  color,
+  unfilledColor,
+  borderColor,
+  borderWidth,
+  style
+}) {
+  return (
+    <View style={[style, { backgroundColor: unfilledColor, borderRadius }]}>
+      <View style={{ backgroundColor: color, width: progress, height: "100%" }} />
+    </View>
+  )
+}

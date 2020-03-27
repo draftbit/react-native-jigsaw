4eac417815b8eef5b07e9805f0b684c1c0e5974f
Peter Piekarczyk <peterpiekarczyk@gmail.com>
2020-03-27 14:54:14 -0500

    fix yarn package resolutions

--- example/metro.config.js
+++ example/metro.config.js
@@ -1,30 +1,42 @@
 // /* eslint-disable import/no-commonjs */
 
+const fs = require("fs")
 const path = require("path")
-const glob = require("glob-to-regexp")
 const blacklist = require("metro-config/src/defaults/blacklist")
-const pak = require("../package.json")
 
-const dependencies = Object.keys(pak.dependencies)
+const root = path.resolve(__dirname, "..")
+const pak = JSON.parse(fs.readFileSync(path.join(root, "package.json"), "utf8"))
+
+const modules = [
+  "@babel/runtime",
+  "@expo/vector-icons",
+  ...Object.keys(pak.dependencies),
+  ...Object.keys(pak.peerDependencies)
+]
 
 module.exports = {
   projectRoot: __dirname,
-  watchFolders: [path.resolve(__dirname, "..")],
+  watchFolders: [root],
 
   resolver: {
     blacklistRE: blacklist([
-      glob(`${path.resolve(__dirname, "..")}/node_modules/*`),
-      glob(`${path.resolve(__dirname, "..")}/example/node_modules/node-fetch/*`),
-      glob(`${path.resolve(__dirname, "..")}/example/node_modules/glob/*`)
+      new RegExp(`^${escape(path.join(root, "node_modules"))}\\/.*$`),
+      new RegExp(`^${escape(path.join(root, "example", "node_modules", "node-fetch"))}\\/.*$`),
+      new RegExp(`^${escape(path.join(root, "example", "node_modules", "glob"))}\\/.*$`)
     ]),
 
-    providesModuleNodeModules: [
-      "react-native",
-      "react",
-      "expo",
-      "@expo/vector-icons",
-      "@babel/runtime",
-      ...dependencies
-    ]
+    extraNodeModules: modules.reduce((acc, name) => {
+      acc[name] = path.join(__dirname, "node_modules", name)
+      return acc
+    }, {}),
+
+    transformer: {
+      getTransformOptions: async () => ({
+        transform: {
+          experimentalImportSupport: false,
+          inlineRequires: true
+        }
+      })
+    }
   }
 }

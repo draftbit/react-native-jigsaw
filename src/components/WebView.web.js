9b27d9b20ec23f0cb30bd4f02fd1d95e84a9434b
Peter Piekarczyk <peterpiekarczyk@gmail.com>
2020-03-27 00:20:47 -0500

    remove all web shit

--- src/components/WebView.web.js
+++
@@ -1,6 +0,0 @@
-import React from "react"
-import UnsupportedView from "./UnsupportedView"
-
-export default function WebView({ aspectRatio, style }) {
-  return <UnsupportedView tag="WebView" style={[style, { aspectRatio }]} />
-}

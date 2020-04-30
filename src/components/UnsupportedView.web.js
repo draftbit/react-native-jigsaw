6a192a54f41e708f55eedd094793d92684b3aa22
Peter Piekarczyk <peterpiekarczyk@gmail.com>
2020-04-29 19:41:39 -0500

    try and fix unsupportedview

--- src/components/UnsupportedView.web.js
+++ src/components/UnsupportedView.web.js
@@ -2,24 +2,37 @@ import React, { useState } from "react"
 import { StyleSheet, Text, ImageBackground } from "react-native"
 import AspectRatio from "./AspectRatio"
 
+function maybeRenderText(layout) {
+  if (layout.width > 100 && layout.height > 100) return true
+  return false
+}
+
 export default function UnsupportedView({ tag, style }) {
   const { aspectRatio, ...extraStyles } = StyleSheet.flatten(style)
   const [layout, setLayout] = useState({})
+
   const message = `${tag} is not supported in Web Preview yet, please use Live Preview`
   if (aspectRatio && extraStyles.position !== "absolute") {
     return (
       <AspectRatio
         ratio={aspectRatio}
-        onLayout={e => ({
-          width: e.nativeEvent.layout.width,
-          height: e.nativeEvent.layout.height
-        })}>
+        onLayout={e => {
+          setLayout({
+            width: e.nativeEvent.layout.width,
+            height: e.nativeEvent.layout.height
+          })
+
+          return {
+            width: e.nativeEvent.layout.width,
+            height: e.nativeEvent.layout.height
+          }
+        }}>
         <ImageBackground
           style={[extraStyles, styles.container, { width: layout.width, height: layout.height }]}
           imageStyle={styles.image}
           source={require("../assets/bg.png")}
           resizeMode="repeat">
-          <Text style={styles.text}>{message}</Text>
+          {maybeRenderText(layout) ? <Text style={styles.text}>{message}</Text> : null}
         </ImageBackground>
       </AspectRatio>
     )
@@ -27,16 +40,23 @@ export default function UnsupportedView({ tag, style }) {
 
   return (
     <ImageBackground
+      onLayout={e => {
+        setLayout({
+          width: e.nativeEvent.layout.width,
+          height: e.nativeEvent.layout.height
+        })
+      }}
       style={[extraStyles, styles.container]}
       source={require("../assets/bg.png")}
       resizeMode="repeat">
-      <Text style={styles.text}>{message}</Text>
+      {maybeRenderText(layout) ? <Text style={styles.text}>{message}</Text> : null}
     </ImageBackground>
   )
 }
 
 const styles = StyleSheet.create({
   container: {
+    overflow: "hidden",
     alignItems: "center",
     justifyContent: "center",
     padding: 10

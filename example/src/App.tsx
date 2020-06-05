b09a48f364e3935427645edeca1142cdba7c8b21
Peter Piekarczyk <peterpiekarczyk@gmail.com>
2020-06-04 19:20:28 -0500

    remove datetimepicker and create @draftbit/datepicker

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -16,8 +16,6 @@ import CarouselExample from "./CarouselExample";
 import ContainerExample from "./ContainerExample";
 import ControllerExample from "./ControllerExample";
 
-import DatePickerExample from "./DatePickerExample";
-
 import FABExample from "./FABExample";
 import FieldSearchBarFullExample from "./FieldSearchBarFullExample";
 
@@ -50,7 +48,6 @@ const ROUTES = {
   Carousel: CarouselExample,
   Container: ContainerExample,
   Controllers: ControllerExample,
-  DatePicker: DatePickerExample,
   FieldSearchBarFull: FieldSearchBarFullExample,
   // TODO fix Header (spacing problem, textOverflow ellipses doesn't work on web)
   Header: HeaderExample,

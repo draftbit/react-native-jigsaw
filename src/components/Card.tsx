0512b4c90c2d8e3a8f0394418f50cc34bb843890
Youssef Henna <youssef.hisham14@gmail.com>
2020-08-22 01:22:52 +0200

    feat(card): convert to typescript

---
+++ src/components/Card.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+import { withTheme } from "../core/theming";
+import Touchable from "./Touchable";
+import Config from "./Config";
+import { StyleProp, ViewStyle } from "react-native";
+
+interface Props {
+  numColumns?: number;
+  children?: React.ReactNode;
+  onPress?: () => void;
+  style?: StyleProp<ViewStyle>;
+  theme: { spacing: { gutters: number; small: number } };
+}
+const Card: React.FC<Props> = ({
+  numColumns = 3,
+  children,
+  onPress,
+  theme: { spacing },
+  style,
+}) => {
+  let cardStyle;
+  if (numColumns === 1) {
+    cardStyle = {
+      width: (Config.windowWidth - spacing.gutters * 2 - spacing.small * 2) / 3,
+    };
+  } else if (numColumns === 2) {
+    cardStyle = {
+      width: (Config.windowWidth - spacing.gutters * 2 - spacing.small) / 2,
+    };
+  } else {
+    cardStyle = { width: Config.windowWidth - spacing.gutters * 2 };
+  }
+
+  return (
+    <Touchable disabled={!onPress} onPress={onPress} style={[cardStyle, style]}>
+      {children}
+    </Touchable>
+  );
+};
+
+export default withTheme(Card);

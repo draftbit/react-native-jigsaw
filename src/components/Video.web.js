b9dd7fe8a1b7e87682954cec594b1e4973273a54
Youssef Henna <youssef.hisham14@gmail.com>
2020-08-25 13:49:29 +0200

    feat(video): convert to typescript

--- src/components/Video.web.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import UnsupportedView from "./UnsupportedView";
-
-export default function Video({ aspectRatio, style }) {
-  return <UnsupportedView tag="Video" style={[style, { aspectRatio }]} />;
-}

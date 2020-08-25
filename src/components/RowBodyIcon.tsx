ee27d11059de63be6dd0585f71af41243a35acfd
Youssef Henna <youssef.hisham14@gmail.com>
2020-08-25 12:41:08 +0200

    feat(rowbodyicon): convert to typescript

--- src/components/RowBodyIcon.tsx
+++ src/components/RowBodyIcon.tsx
@@ -9,38 +9,44 @@ import {
 import Row from "./Row";
 import Icon from "./Icon";
 import Config from "./Config";
+import theme from "../styles/DefaultTheme";
+import { StyleProp, ViewStyle } from "react-native";
 
-class RowBodyIcon extends React.Component {
-  render() {
-    const {
-      title,
-      subtitle,
-      icon,
-      style,
-      theme: { colors, typography, spacing },
-    } = this.props;
-
-    return (
-      <Row
-        titleTypeStyle={typography.body1}
-        titleColor={colors.medium}
-        subtitleTypeStyle={typography.subtitle2}
-        subtitleColor={colors.light}
-        title={title}
-        subtitle={subtitle}
-        right={() => (
-          <Icon
-            name={icon}
-            size={Config.rowSingleLineIconSize}
-            color={colors.light}
-            style={{ marginLeft: spacing.large }}
-          />
-        )}
-        style={style}
-      />
-    );
-  }
+interface Props {
+  title?: string;
+  subtitle?: string;
+  icon: string;
+  style?: StyleProp<ViewStyle>;
+  theme: typeof theme;
 }
+
+const RowBodyIcon: React.FC<Props> = ({
+  title,
+  subtitle,
+  icon,
+  style,
+  theme: { colors, typography, spacing },
+}) => {
+  return (
+    <Row
+      titleTypeStyle={typography.body1}
+      titleColor={colors.medium}
+      subtitleTypeStyle={typography.subtitle2}
+      subtitleColor={colors.light}
+      title={title}
+      subtitle={subtitle}
+      right={() => (
+        <Icon
+          name={icon}
+          size={Config.rowSingleLineIconSize}
+          color={colors.light}
+          style={{ marginLeft: spacing.large }}
+        />
+      )}
+      style={style}
+    />
+  );
+};
 
 export default withTheme(RowBodyIcon);
 

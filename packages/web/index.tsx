3294f2040c9823c89a5eed42d1515381061eab54
David Sancho <dsnxmoreno@gmail.com>
2021-01-26 22:31:55 +0100

    docs: Explain why we don't want to rely on main

--- packages/web/index.tsx
+++ packages/web/index.tsx
@@ -1,5 +1,5 @@
 /* This points directly to src/index instead of "@draftbit/ui" because we don't want to rely
-on the package.json "main" */
+on the package.json "main" since it needs to point to "lib/commonjs/index.js" to keep functionality with bob as described here: https://github.com/callstack/react-native-builder-bob#manual-configuration and we want to go to the entrypoint in TypeScript here. */
 import * as JigsawUI from "../react-native-jigsaw/src/index";
 
 const JigsawWeb: any = {

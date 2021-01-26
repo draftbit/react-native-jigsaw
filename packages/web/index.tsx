51bebe9408c2e84d42ea9d1de89300b536d0b5d9
David Sancho <dsnxmoreno@gmail.com>
2021-01-26 19:10:36 +0100

    docs: Add explanation in ui/index.tsx

--- packages/web/index.tsx
+++ packages/web/index.tsx
@@ -1,3 +1,5 @@
+/* This points directly to src/index instead of "@draftbit/ui" because we don't want to rely
+on the package.json "main" */
 import * as JigsawUI from "../react-native-jigsaw/src/index";
 
 const JigsawWeb: any = {

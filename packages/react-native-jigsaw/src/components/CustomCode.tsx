2268563c01e251898dc637c65ebcc7c743ed17a2
Donald Hruska <donaldhruska@gmail.com>
2021-02-25 14:30:18 -0600

    feat(custom code): create custom code component

---
+++ packages/react-native-jigsaw/src/components/CustomCode.tsx
@@ -0,0 +1,9 @@
+import { COMPONENT_TYPES } from "../core/component-types";
+
+export const SEED_DATA = {
+  name: "Custom Code",
+  tag: "CustomCode",
+  description: "A component that allows usage of custom code",
+  category: COMPONENT_TYPES.codeComponent,
+  props: {},
+};

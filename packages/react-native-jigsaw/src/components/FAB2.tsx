3e0a91f516d0790624aac1bb77551c5ab5bd2e49
Peter Piekarczyk <peterpiekarczyk@gmail.com>
2021-03-16 15:45:55 -0500

    fab

--- packages/react-native-jigsaw/src/components/FAB2.tsx
+++ packages/react-native-jigsaw/src/components/FAB2.tsx
@@ -1,18 +1,36 @@
 import * as React from "react";
-import { View, StyleSheet, Pressable, ActivityIndicator } from "react-native";
+import {
+  View,
+  StyleSheet,
+  Pressable,
+  ActivityIndicator,
+  StyleProp,
+  ViewStyle,
+  PressableProps,
+} from "react-native";
 import { withTheme } from "../core/theming";
 import Icon from "./Icon";
+import theme from "../styles/DefaultTheme";
+
+import {
+  GROUPS,
+  FORM_TYPES,
+  PROP_TYPES,
+  COMPONENT_TYPES,
+} from "../core/component-types";
 
 type Props = {
   disabled?: boolean;
   loading?: boolean;
-  iconName?: string;
+  size?: number;
+  bgColor?: string;
   iconColor?: string;
+  iconName?: string;
   onPress: () => void;
   theme: typeof theme;
   IconOverride: typeof Icon;
   style?: StyleProp<ViewStyle>;
-} & TouchableHighlightProps;
+} & PressableProps;
 
 const FAB: React.FC<Props> = ({
   onPress,
@@ -48,8 +66,6 @@ const FAB: React.FC<Props> = ({
           return [
             styles.button,
             {
-              bottom: "5%",
-              right: "10%",
               opacity: pressed ? 0.75 : 1,
               width: size,
               height: size,
@@ -61,16 +77,13 @@ const FAB: React.FC<Props> = ({
         }}
         {...props}
       >
-        {!loading ? (
-          <ActivityIndicator size="small" color={iconColor} />
-        ) : (
-          <SelectedIcon
-            name={iconName}
-            size={28}
-            color={iconColor}
-            style={styles.icon}
-          />
-        )}
+        <View style={styles.icon}>
+          {!loading ? (
+            <ActivityIndicator size="small" color={iconColor} />
+          ) : (
+            <SelectedIcon name={iconName} size={28} color={iconColor} />
+          )}
+        </View>
       </Pressable>
     </View>
   );
@@ -78,14 +91,68 @@ const FAB: React.FC<Props> = ({
 
 const styles = StyleSheet.create({
   button: {
-    position: "absolute",
+    // @ts-ignore
     cursor: "pointer",
     justifyContent: "center",
     alignItems: "center",
   },
   icon: {
+    // @ts-ignore
     userSelect: "none",
   },
 });
 
 export default withTheme(FAB);
+
+export const SEED_DATA = [
+  {
+    name: "FAB Mini",
+    tag: "FAB",
+    category: COMPONENT_TYPES.button,
+    description: "A round, mini FAB",
+    preview_image_url: "{CLOUDINARY_URL}/Button_FABMini.png",
+    props: {
+      icon: {
+        group: GROUPS.basic,
+        label: "Icon Name",
+        description: "Name of the icon",
+        editable: true,
+        required: true,
+        formType: FORM_TYPES.icon,
+        propType: PROP_TYPES.ASSET,
+        defaultValue: null,
+      },
+      bgColor: {
+        group: GROUPS.basic,
+        label: "Color Override",
+        description: "Override the background color of the button",
+        editable: true,
+        required: false,
+        formType: FORM_TYPES.color,
+        propType: PROP_TYPES.THEME,
+        defaultValue: null,
+      },
+      iconColor: {
+        group: GROUPS.basic,
+        label: "Color Override",
+        description: "Override the background color of the button",
+        editable: true,
+        required: false,
+        formType: FORM_TYPES.color,
+        propType: PROP_TYPES.THEME,
+        defaultValue: null,
+      },
+      onPress: {
+        group: GROUPS.basic,
+        label: "Action",
+        description: "Action to execute when button pressed",
+        editable: true,
+        required: false,
+        formType: FORM_TYPES.action,
+        propType: PROP_TYPES.STRING,
+        defaultValue: null,
+      },
+    },
+    layout: {},
+  },
+];

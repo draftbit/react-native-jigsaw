b048044cc78d12950c40277a8f5b8e0666e36e16
Peter Piekarczyk <peterpiekarczyk@gmail.com>
2021-03-16 18:14:14 -0500

    chore(fab): remove spacing/elevation

--- packages/example/src/FABExample.js
+++ packages/example/src/FABExample.js
@@ -3,69 +3,13 @@ import { withTheme, FAB } from "@draftbit/ui";
 import Section, { Container, styles } from "./Section";
 
 function FABExample({ theme }) {
-  // const [elevation, setElevation] = React.useState(0);
-
   const handlePress = () => {};
   return (
     <Container style={{ backgroundColor: theme.colors.background }}>
       <Section title="Solid" style={styles.row}>
-        <FAB type="standard" icon="add" onPress={handlePress} />
-        <FAB type="standard" icon="add" disabled onPress={handlePress} />
-        <FAB type="standard" icon="add" loading onPress={handlePress} />
-      </Section>
-
-      <Section title="Extended" style={styles.row}>
-        <FAB type="extended" label="Extended FAB" onPress={handlePress} />
-        <FAB
-          type="extended"
-          icon="add"
-          label="Extended FAB"
-          onPress={handlePress}
-        />
-        <FAB
-          type="extended"
-          icon="add"
-          label="Extended Disabled FAB"
-          disabled
-          onPress={handlePress}
-        />
-        <FAB
-          type="extended"
-          icon="add"
-          label="Extended Loading FAB"
-          loading
-          onPress={handlePress}
-        />
-      </Section>
-
-      <Section title="Outline">
-        <FAB type="outline" icon="add" onPress={handlePress} />
-        <FAB type="outline" icon="add" disabled onPress={handlePress} />
-        <FAB type="outline" icon="add" loading onPress={handlePress} />
-      </Section>
-
-      <Section title="Fixed">
-        <FAB type="fixed" label="Fixed FAB" onPress={handlePress} />
-        <FAB
-          type="fixed"
-          icon="alarm"
-          label="Fixed FAB"
-          onPress={handlePress}
-        />
-        <FAB
-          type="fixed"
-          icon="alarm"
-          label="Fixed Disabled FAB"
-          disabled
-          onPress={handlePress}
-        />
-        <FAB
-          type="fixed"
-          icon="alarm"
-          label="Fixed Loading FAB"
-          loading
-          onPress={handlePress}
-        />
+        <FAB icon="MaterialIcons/add" onPress={handlePress} />
+        <FAB icon="MaterialIcons/add" disabled onPress={handlePress} />
+        <FAB icon="MaterialIcons/add" loading onPress={handlePress} />
       </Section>
     </Container>
   );

a09a0a37d1b59db44c29ac7b6d95e3653e97b4f9
Youssef Henna <youssef.hisham14@gmail.com>
2021-05-05 14:51:48 +0200

    chore(togglebutton): added SEED_DATA

--- packages/react-native-jigsaw/src/components/ToggleButton.tsx
+++ packages/react-native-jigsaw/src/components/ToggleButton.tsx
@@ -4,6 +4,19 @@ import themeT from "../styles/DefaultTheme";
 import { colorTypes } from "../types";
 import { StyleProp, StyleSheet, ViewStyle } from "react-native";
 import IconButton from "./IconButton";
+import {
+  COMPONENT_TYPES,
+  GROUPS,
+  createIconProp,
+  FORM_TYPES,
+  PROP_TYPES,
+  createBoolProp,
+  createTextProp,
+  createColorProp,
+  createNumberProp,
+  createActionProp,
+  FIELD_NAME,
+} from "../core/component-types";
 
 type Props = {
   icon: string;
@@ -68,3 +81,59 @@ const styles = StyleSheet.create({
 });
 
 export default withTheme(ToggleButton);
+
+export const SEED_DATA = {
+  name: "Toggle Button",
+  tag: "ToggleButton",
+  category: COMPONENT_TYPES.button,
+  layout: {},
+  props: {
+    icon: createIconProp({
+      required: true,
+    }),
+    fieldName: {
+      ...FIELD_NAME,
+      defaultValue: "unchecked",
+      valuePropName: "status",
+      handlerPropName: "onPress",
+    },
+    onPress: createActionProp(),
+    disabled: createBoolProp({
+      label: "Disabled",
+      description: "Whether the button should be disabled",
+    }),
+    color: createColorProp(),
+    colorSecondary: createColorProp({
+      label: "Secondary Color",
+    }),
+    borderColor: createColorProp({
+      label: "Border Color",
+    }),
+    iconSize: {
+      group: GROUPS.basic,
+      label: "Icon Size",
+      description: "Size of icon",
+      editable: true,
+      required: false,
+      formType: FORM_TYPES.flatArray,
+      propType: PROP_TYPES.NUMBER,
+      defaultValue: 32,
+      options: [12, 16, 24, 32, 48, 64],
+    },
+    width: createNumberProp({
+      label: "Width",
+      description: "Width",
+      defaultValue: 50,
+    }),
+    height: createNumberProp({
+      label: "Height",
+      description: "Height",
+      defaultValue: 50,
+    }),
+    accessibilityLabel: createTextProp({
+      label: "Accessibility Label",
+      description:
+        "Overrides the text that's read by the screen reader when the user interacts with the element. By default, the label is constructed by traversing all the children and accumulating all the Text nodes separated by space.",
+    }),
+  },
+};

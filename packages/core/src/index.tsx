d88ce82161e1b11e0a572884cabf4489a38ed45c
Peter Piekarczyk <peterpiekarczyk@gmail.com>
2021-05-21 13:56:11 -0500

    update useAuthState

--- packages/core/src/index.tsx
+++ packages/core/src/index.tsx
@@ -61,4 +61,4 @@ export { default as RowHeadlineImageCaption } from "./components/RowHeadlineImag
 export { default as RowHeadlineImageIcon } from "./components/RowHeadlineImageIcon";
 export { default as Slider } from "./components/Slider";
 export { default as Stepper } from "./components/Stepper";
-export { useAuthState } from "./core/useAuthState";
+export { useAuthState } from "./components/useAuthState";

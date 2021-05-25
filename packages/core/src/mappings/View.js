b27199e7d7540c31c5fc260be3b621df267d2c04
Peter Piekarczyk <peterpiekarczyk@gmail.com>
2021-05-25 09:51:52 -0500

    fix view seed_data props

--- packages/core/src/mappings/View.js
+++ packages/core/src/mappings/View.js
@@ -21,7 +21,7 @@ export const SEED_DATA = {
       required: false,
       formType: FORM_TYPES.boolean,
       propType: PROP_TYPES.BOOLEAN,
-      defaultValue: true,
+      defaultValue: null,
     },
     accessibilityLabel: {
       group: GROUPS.accessibility,
@@ -98,7 +98,7 @@ export const SEED_DATA = {
       required: false,
       formType: FORM_TYPES.boolean,
       propType: PROP_TYPES.BOOLEAN,
-      defaultValue: false,
+      defaultValue: null,
     },
     accessibilityIgnoresInvertColors: {
       group: GROUPS.accessibility,
@@ -129,7 +129,7 @@ export const SEED_DATA = {
       group: GROUPS.accessibility,
       name: "importantForAccessibility",
       label: "importantForAccessibility",
-      defaultValue: "auto",
+      defaultValue: null,
       description:
         "Controls how view is important for accessibility which is if it fires accessibility events and if it is reported to accessibility services that query the screen. Works for Android only.Possible values:\n'auto' - The system determines whether the view is important for accessibility - default (recommended).\n'yes' - The view is important for accessibility.\n'no' - The view is not important for accessibility.\n'no-hide-descendants' - The view is not important for accessibility, nor are any of its descendant views.\nSee the Android importantForAccessibility docs for reference.",
       options: ["auto", "yes", "no", "no-hide-descendants"],
@@ -148,7 +148,7 @@ export const SEED_DATA = {
       required: false,
       formType: FORM_TYPES.position,
       propType: PROP_TYPES.OBJECT,
-      defaultValue: "{}",
+      defaultValue: null,
     },
     pointerEvents: {
       group: GROUPS.advanced,
@@ -195,9 +195,9 @@ export const SEED_DATA = {
         "Whether this View needs to rendered offscreen and composited with an alpha in order to preserve 100% correct colors and blending behavior. The default (false) falls back to drawing the component and its children with an alpha applied to the paint used to draw each element instead of rendering the full component offscreen and compositing it back with an alpha value. This default may be noticeable and undesired in the case where the View you are setting an opacity on has multiple overlapping elements (e.g. multiple overlapping Views, or text and a background).Rendering offscreen to preserve correct alpha behavior is extremely expensive and hard to debug for non-native developers, which is why it is not turned on by default. If you do need to enable this property for an animation, consider combining it with renderToHardwareTextureAndroid if the view contents are static (i.e. it doesn't need to be redrawn each frame). If that property is enabled, this View will be rendered off-screen once, saved in a hardware texture, and then composited onto the screen with an alpha each frame without having to switch rendering targets on the GPU.",
       editable: true,
       required: false,
-      defaultValue: false,
       formType: FORM_TYPES.boolean,
       propType: PROP_TYPES.BOOLEAN,
+      defaultValue: null,
     },
     renderToHardwareTextureAndroid: {
       group: GROUPS.advanced,

138541bea0fb6cc741485a2eaf396268653d8ba6
Peter Piekarczyk <peterpiekarczyk@gmail.com>
2021-05-25 20:15:57 -0500

    switch updates

--- packages/core/src/components/Switch.tsx
+++ packages/core/src/components/Switch.tsx
@@ -1,35 +1,45 @@
 import * as React from "react";
-import { Switch as NativeSwitch, Platform, SwitchProps } from "react-native";
+import { Switch as NativeSwitch, SwitchProps } from "react-native";
 import { withTheme } from "../theming";
 import {
   COMPONENT_TYPES,
   createBoolProp,
   createColorProp,
+  createFieldNameProp,
+  colorTypes,
 } from "@draftbit/types";
-import themeT from "../styles/DefaultTheme";
+import type { Theme } from "../styles/DefaultTheme";
 
 type Props = {
   value: boolean;
   disabled?: boolean;
   onValueChange?: (value: boolean) => void;
-  color?: string;
-  theme: typeof themeT;
+  theme: Theme;
+  activeTrackColor: colorTypes;
+  inactiveTrackColor: colorTypes;
+  activeThumbColor: colorTypes;
+  inactiveThumbColor: colorTypes;
 } & SwitchProps;
 
-const Switch: React.FC<Props> = ({
+function Switch({
   value = false,
   disabled,
   onValueChange,
-  color,
+  activeTrackColor,
+  inactiveTrackColor,
+  activeThumbColor,
+  inactiveThumbColor,
   theme,
   style,
-  ...props
-}) => {
-  let [checked, setChecked] = React.useState(value);
-  let thumbColor = "#FFF";
-  const checkedColor = color || theme.colors.primary;
-  const uncheckedColor = "#ddd";
+  ...rest
+}: Props) {
+  const activeTrackThemeColor = theme.colors[activeTrackColor];
+  const inactiveTrackThemeColor = theme.colors[inactiveTrackColor];
 
+  const activeThumbThemeColor = theme.colors[activeThumbColor];
+  const inactiveThumbThemeColor = theme.colors[inactiveThumbColor];
+
+  const [checked, setChecked] = React.useState(value);
   React.useEffect(() => {
     if (value !== checked) {
       setChecked(value);
@@ -38,27 +48,25 @@ const Switch: React.FC<Props> = ({
 
   return (
     <NativeSwitch
-      {...props}
       value={checked}
       disabled={disabled}
-      trackColor={{ false: uncheckedColor, true: checkedColor }}
-      thumbColor={thumbColor}
-      onValueChange={(boolValue) => {
-        if (!disabled) {
-          setChecked(boolValue);
-          onValueChange && onValueChange(boolValue);
-        }
+      trackColor={{
+        false: inactiveTrackThemeColor,
+        true: activeTrackThemeColor,
       }}
-      style={[
-        {
-          opacity:
-            disabled && Platform.OS !== "ios" ? theme.disabledOpacity : 1,
-        },
-        style,
-      ]}
+      thumbColor={value ? activeThumbThemeColor : inactiveThumbThemeColor}
+      // @ts-ignore react-native-web only
+      activeThumbColor={activeThumbThemeColor}
+      ios_backgroundColor={inactiveTrackThemeColor}
+      style={style}
+      onValueChange={(bool) => {
+        setChecked(bool);
+        onValueChange && onValueChange(bool);
+      }}
+      {...rest}
     />
   );
-};
+}
 
 export default withTheme(Switch);
 
@@ -76,6 +84,21 @@ export const SEED_DATA = {
       label: "Value",
       description: "Boolean value",
     }),
-    color: createColorProp(),
+    fieldName: createFieldNameProp({
+      defaultValue: false,
+      valuePropName: "switchValue",
+    }),
+    activeTrackColor: createColorProp({
+      label: "Active Track Color",
+    }),
+    inactiveTrackColor: createColorProp({
+      label: "Active Track Color",
+    }),
+    activeThumbColor: createColorProp({
+      label: "Active Thumb Color",
+    }),
+    inactiveThumbColor: createColorProp({
+      label: "Active Thumb Color",
+    }),
   },
 };

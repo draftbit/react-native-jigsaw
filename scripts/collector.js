b926193f488e2b8060303fa60f02566992d1bce7
Peter Piekarczyk <peterpiekarczyk@gmail.com>
2021-06-11 12:11:26 -0500

    undo scripts collector changes

--- scripts/collector.js
+++ scripts/collector.js
@@ -21,47 +21,33 @@ const IGNORED_FILES = [
 const ERROR_FILES = [];
 const COMPLETED_FILES = [];
 
-async function main(list) {
+async function main() {
   console.log("Running on", getUrl());
-
-  if (list) {
-    console.log("ONLY running", JSON.stringify(list));
-  }
 
   const nativeFiles = await globAsync(`${NATIVE_PATH}/**/*.tsx`);
   const componentFiles = await globAsync(`${COMPONENT_PATH}/**/*.tsx`);
   const screenFiles = await globAsync(`${SCREEN_PATH}/**/*.tsx`);
   const mappingFiles = await globAsync(`${MAPPING_PATH}/**/*.js`);
 
-  const components = list.map((file) => {
-    if (!file.includes("ts")) {
-      throw Error("Must include extension: tsx or js");
+  const files = [
+    ...nativeFiles,
+    ...componentFiles,
+    ...screenFiles,
+    ...mappingFiles,
+  ].filter((file) => {
+    const name = file.split("/").pop();
+
+    if (
+      name.includes("web") ||
+      name.includes("ios") ||
+      name.includes("android")
+    ) {
+      console.log(`Ignoring... ${name}`);
+      return false;
     }
 
-    return `${COMPONENT_PATH}/${file}`;
+    return !IGNORED_FILES.includes(name);
   });
-
-  const files = Array.isArray(components)
-    ? components
-    : [
-        ...nativeFiles,
-        ...componentFiles,
-        ...screenFiles,
-        ...mappingFiles,
-      ].filter((file) => {
-        const name = file.split("/").pop();
-
-        if (
-          name.includes("web") ||
-          name.includes("ios") ||
-          name.includes("android")
-        ) {
-          console.log(`Ignoring... ${name}`);
-          return false;
-        }
-
-        return !IGNORED_FILES.includes(name);
-      });
 
   for (const file of files) {
     const [name, category] = file.split("/").reverse();
@@ -105,4 +91,4 @@ async function uploadComponent(component) {
   });
 }
 
-main(process.argv.slice(2));
+main();

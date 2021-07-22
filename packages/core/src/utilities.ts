9177c3128be9697a2830bd52f52211ab82cda618
Donald Hruska <donaldhruska@gmail.com>
2021-07-22 08:58:26 -0700

    Update packages/core/src/utilities.ts

--- packages/core/src/utilities.ts
+++ packages/core/src/utilities.ts
@@ -37,6 +37,13 @@ export function extractStyles(style: StyleProp<any>) {
  * Merges a style object on top of another style object. In React Native,
  * keys with undefined values in a style object will still override styles
  * that appear earlier in a sequence. This avoids that problem.
+ *
+ * This lets us avoid the `...(something ? { something } : {})` pattern.
+ * There doesn't seem to be a better way to do this. These all seem to not
+ * work (i.e. they all result in `{ color: undefined }`:
+ * `const mergedStyles = [{ color: "red" }, { color: undefined }]`
+ * `const mergedStyles = StyleSheet.compose({ color: "red" }, { color: undefined })`
+ * `const mergedStyles = StyleSheet.flatten([{ color: "red" }, { color: undefined }])`
  */
 export function applyStyles(
   baseStyles: StyleProp<any>,

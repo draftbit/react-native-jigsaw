8523df7639782b089284e6e0e745ab6372104149
Gunjan Kothari <gunjan.kothari2002@gmail.com>
2021-08-20 19:58:16 +0530

    Removed slider margin

--- packages/core/src/components/Slider.tsx
+++ packages/core/src/components/Slider.tsx
@@ -122,8 +122,6 @@ const styles = StyleSheet.create({
     height: 40,
     flexDirection: "row",
     alignItems: "center",
-    marginLeft: 12,
-    marginRight: 12,
   },
   slider: {
     flex: 1,

d25e1f01bac9f1a714e7ef9b9aa5ec6bd21faeca
Gunjan Kothari <gunjan.kothari2002@gmail.com>
2021-09-30 00:20:26 +0530

    swiper component (#311)

--- packages/core/src/components/Swiper/Swiper.tsx
+++ packages/core/src/components/Swiper/Swiper.tsx
@@ -3,16 +3,11 @@ import {
   createBoolProp,
   createNumberProp,
   createTextProp,
+  GROUPS,
 } from "@draftbit/types";
 import React from "react";
-import { View, StyleSheet, StyleProp, ViewStyle } from "react-native";
+import { View, StyleProp, ViewStyle } from "react-native";
 import SwiperComponent from "react-native-web-swiper";
-
-const styles = StyleSheet.create({
-  wrapper: {
-    flex: 1,
-  },
-});
 
 export interface SwiperProps {
   vertical?: boolean;
@@ -37,7 +32,7 @@ const Swiper = ({
   children,
   style,
 }: SwiperProps) => (
-  <View style={[styles.wrapper, style]}>
+  <View style={style}>
     <SwiperComponent
       from={from}
       loop={loop}
@@ -61,30 +56,41 @@ export const SEED_DATA = {
   tag: "Swiper",
   description: "Swiper container",
   category: COMPONENT_TYPES.container,
+  layout: {
+    height: 300,
+    width: "100%",
+  },
   props: {
     from: createNumberProp({
+      group: GROUPS.basic,
       label: "Initial Slide",
     }),
     loop: createBoolProp({
+      group: GROUPS.basic,
       label: "Loop",
     }),
     timeout: createNumberProp({
+      group: GROUPS.basic,
       label: "Timeout",
       defaultValue: 0,
     }),
     vertical: createBoolProp({
+      group: GROUPS.basic,
       label: "Vertical",
       defaultValue: false,
     }),
     prevTitle: createTextProp({
+      group: GROUPS.basic,
       label: "Previous Title",
       defaultValue: "",
     }),
     nextTitle: createTextProp({
+      group: GROUPS.basic,
       label: "Next Title",
       defaultValue: "",
     }),
     dotsTouchable: createBoolProp({
+      group: GROUPS.basic,
       label: "Dots Touchable",
       defaultValue: true,
     }),

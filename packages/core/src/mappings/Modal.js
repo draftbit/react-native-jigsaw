df0293967c0b0a032dd605fd321be6a19f454f4e
Gunjan Kothari <gunjan.kothari2002@gmail.com>
2021-10-06 01:48:08 +0530

    Modal component

---
+++ packages/core/src/mappings/Modal.js
@@ -0,0 +1,38 @@
+import {
+  GROUPS,
+  COMPONENT_TYPES,
+  FORM_TYPES,
+  PROP_TYPES,
+  createBoolProp,
+} from "@draftbit/types";
+
+export const SEED_DATA = {
+  name: "Modal",
+  tag: "Modal",
+  description: "A basic Modal Component",
+  category: COMPONENT_TYPES.container,
+  props: {
+    animationType: {
+      group: GROUPS.advanced,
+      name: "animationType",
+      label: "animationType",
+      description: "Animatio Type",
+      options: ["slide", "fade", "none"],
+      editable: false,
+      required: false,
+      formType: FORM_TYPES.flatArray,
+      propType: PROP_TYPES.STRING,
+      defaultValue: "none",
+    },
+    transparent: createBoolProp({
+      group: GROUPS.basic,
+      label: "Transparent",
+      description: "Should be Transparent",
+    }),
+    visible: createBoolProp({
+      group: GROUPS.data,
+      label: "Visible",
+      description: "Should be Visible",
+    }),
+  },
+};

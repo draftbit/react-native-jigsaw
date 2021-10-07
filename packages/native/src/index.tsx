3ca33ac322a82878439f287e8f6916456f9e48b0
Peter Piekarczyk <peterpiekarczyk@gmail.com>
2021-10-07 12:33:42 -0500

    updates

--- packages/native/src/index.tsx
+++ packages/native/src/index.tsx
@@ -1,3 +1,4 @@
 export { default as AudioPlayer } from "./components/AudioPlayer";
 export { default as Icon } from "./components/Icon";
 export { default as WebView } from "./components/WebView";
+// MapView deprecated

72a6d1c7374b04885c49977ed5bd859169c60361
Donald Hruska <donaldhruska@gmail.com>
2021-11-16 13:18:14 -0800

    Move all seed data into mapping files and convert to TS (#353)
    * Convert existing mappings to ts
    
    * move over remaining seed data
    
    * rm comment

---
+++ packages/core/src/mappings/ActionSheetCancel.ts
@@ -0,0 +1,27 @@
+import {
+  COMPONENT_TYPES,
+  createActionProp,
+  createColorProp,
+  createTextProp,
+  GROUPS,
+  Triggers,
+} from "@draftbit/types";
+
+export const SEED_DATA = {
+  name: "Action Sheet Cancel",
+  tag: "ActionSheetCancel",
+  description: "Action Sheet cancel",
+  category: COMPONENT_TYPES.button,
+  triggers: [Triggers.OnPress],
+  props: {
+    onPress: createActionProp(),
+    label: createTextProp({
+      group: GROUPS.basic,
+      label: "Label",
+      defaultValue: "Cancel",
+    }),
+    color: createColorProp({
+      label: "Font Color",
+    }),
+  },
+};

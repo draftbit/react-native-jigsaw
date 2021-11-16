8ca1834e0ea5fb34d49d6ba6e1cf73ca2f11b00b
Gunjan Kothari <gunjan@leverture.com>
2021-11-17 01:31:01 +0530

    P-2597 Surface flex prop style fix

--- packages/core/src/components/Surface.tsx
+++ packages/core/src/components/Surface.tsx
@@ -6,7 +6,6 @@ import {
   ViewProps,
   StyleProp,
   ViewStyle,
-  View,
 } from "react-native";
 import shadow from "../styles/shadow";
 import overlay from "../styles/overlay";
@@ -48,7 +47,7 @@ const Surface: React.FC<Props> = ({
         style,
       ]}
     >
-      <View style={{ overflow: "hidden", borderRadius }}>{children}</View>
+      {children}
     </Animated.View>
   );
 };

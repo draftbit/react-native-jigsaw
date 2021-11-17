b1437fad70f42bcd1ed067a5a8b3a4bf60217f5e
Donald Hruska <donaldhruska@gmail.com>
2021-11-17 00:01:38 -0800

    modify ui package to try to force deployment

--- packages/ui/src/Maps/Maps.native.tsx
+++ packages/ui/src/Maps/Maps.native.tsx
@@ -1,1 +1,2 @@
+// Export the react-native-maps components
 export { MapCallout, MapMarker, MapView } from "@draftbit/native";
--- packages/ui/src/Maps/Maps.web.tsx
+++ packages/ui/src/Maps/Maps.web.tsx
@@ -1,1 +1,2 @@
+// Export the web-based version of the Maps components
 export { MapCallout, MapMarker, MapView } from "@draftbit/core";

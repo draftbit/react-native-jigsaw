038314de5e62a7f5465b87cf98a6b8439c53e7fc
Peter Piekarczyk <peterpiekarczyk@gmail.com>
2021-11-19 13:03:05 -0600

    deletes all maps in 1 commit so we can reference it - right now it makes references to draftbit/native and core

--- packages/maps/src/index.d.ts
+++
@@ -1,2 +0,0 @@
-export { default as AudioPlayer } from "./components/AudioPlayer";
-export { default as Icon } from "./components/Icon";
--- packages/maps/src/index.tsx
+++
@@ -1,4 +0,0 @@
-export { default as AudioPlayer } from "./components/AudioPlayer";
-export { default as Icon } from "./components/Icon";
-export { default as WebView } from "./components/WebView";
-export { MapView, MapMarker, MapCallout } from "./components/MapView";

5e615b3a2667d9ce02d7eaff4d0ed088d7f6ae19
Peter Piekarczyk <peterpme@users.noreply.github.com>
2021-11-19 13:25:31 -0600

    move maps to @draftbit/maps (#363)
    * move maps to @draftbit/maps
    
    * remove example
    
    * remove maps for now
    
    * cleanup
    
    * deletes all maps in 1 commit so we can reference it - right now it makes references to draftbit/native and core
    
    Co-authored-by: Peter Piekarczyk <peterpiekarczyk@gmail.com>

--- packages/core/src/index.tsx
+++ packages/core/src/index.tsx
@@ -32,7 +32,6 @@ export {
   ActionSheetCancel,
 } from "./components/ActionSheet";
 export { Swiper, SwiperItem } from "./components/Swiper";
-export { MapView, MapMarker, MapCallout } from "./components/MapView";
 
 export {
   Center,

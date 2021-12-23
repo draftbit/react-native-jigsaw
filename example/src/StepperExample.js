a0fd590346f1db7cf8ba89238850d289588164c0
Gunjan Kothari <gunjan.kothari2002@gmail.com>
2021-12-24 01:25:57 +0530

    stepper component - min, max value support

--- example/src/StepperExample.js
+++ example/src/StepperExample.js
@@ -5,16 +5,18 @@ import Section, { Container } from "./Section";
 function StepperExample({ theme }) {
   const [value, setValue] = React.useState(0);
   const [value2, setValue2] = React.useState(0);
+  // const [value3, setValue3] = React.useState(0);
+  const [value4, setValue4] = React.useState(0);
   const handleChange = (v) => setValue(v);
 
   return (
     <Container style={{ backgroundColor: theme.colors.background }}>
       <Section title="With State">
-        <Stepper value={value} onChange={handleChange} />
+        <Stepper value={value} onChange={handleChange} min={5} max={10} />
       </Section>
 
       <Section title="Static">
-        <Stepper value={value} />
+        <Stepper value={value2} />
       </Section>
 
       <Section title="No Value">
@@ -23,8 +25,8 @@ function StepperExample({ theme }) {
 
       <Section title="With State and defaultValue">
         <Stepper
-          value={value2}
-          onChange={(v) => setValue2(v)}
+          value={value4}
+          onChange={(v) => setValue4(v)}
           defaultValue={100}
         />
       </Section>

fa1625faf3c5c858abb7e7b856c3193137e07ef2
Shawn Michael Larkin <shawn.michael.larkin@gmail.com>
2022-01-31 16:02:37 -0500

    Refactor NumberInput

--- packages/core/src/components/NumberInput.tsx
+++ packages/core/src/components/NumberInput.tsx
@@ -1,55 +1,73 @@
-import React from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import { TextInput } from "react-native";
+import { isString, isNumber } from "lodash";
 
 interface Props {
-  value?: number;
-  defaultValue?: number;
-  onChangeText: (value?: number) => void;
+  value?: number | string;
+  defaultValue?: number | string;
+  onChangeText?: (value?: number) => void;
 }
 
 const NumberInput: React.FC<Props> = ({
   onChangeText,
-  value,
-  defaultValue,
+  value = "",
+  defaultValue = "",
   ...props
 }) => {
-  const [isDecimal, setIsDecimal] = React.useState(
-    value && !Number.isInteger(value)
-  );
-  React.useEffect(() => {
-    if (value) {
-      setIsDecimal(value.toString().includes("."));
-    }
-  }, [value]);
+  const [isDecimal, setIsDecimal] = useState(value && !Number.isInteger(value));
 
-  const handleChangeText = (newValue: string) => {
-    if (onChangeText) {
-      const parsedNumber = parseFloat(newValue);
-      const number = isNaN(parsedNumber) ? 0 : parsedNumber;
-      setIsDecimal(newValue.includes("."));
-      onChangeText(number);
+  const formatValueToString = useCallback(
+    (valueToStringify?: number | string) => {
+      if (valueToStringify != null) {
+        if (
+          isString(valueToStringify) &&
+          /^[+-]?([0-9]+\.?[0-9]*|\.[0-9]+)$/.test(valueToStringify)
+        ) {
+          console.log("is string", value);
+          return valueToStringify;
+        } else if (isNumber(valueToStringify)) {
+          return valueToStringify.toString();
+        }
+      }
+
+      return "";
+    },
+    [value]
+  );
+
+  useEffect(() => {
+    if (value) {
+      setIsDecimal(formatValueToString(value).includes("."));
     }
+  }, [value, formatValueToString]);
+
+  const makeFinalValue = (valueToMakeFinal?: number | string) => {
+    let stringValue = formatValueToString(valueToMakeFinal);
+
+    if (isDecimal && !stringValue.includes(".")) {
+      stringValue = `${stringValue}.`;
+    }
+
+    return stringValue;
   };
 
-  let strValue;
-  if (value != undefined) {
-    strValue = value.toString();
-    if (isDecimal && !strValue.includes(".")) {
-      strValue = `${strValue}.`;
-    }
-  }
+  const handleChangeText = (newValue: string) => {
+    const parsedNumber = parseFloat(newValue);
+    const number = isNaN(parsedNumber) ? 0 : parsedNumber;
+
+    setIsDecimal(newValue.includes("."));
+    onChangeText?.(number);
+  };
 
   return (
     <TextInput
       keyboardType="numeric"
       {...props}
-      value={strValue}
-      defaultValue={defaultValue?.toString()}
+      value={makeFinalValue(value)}
+      defaultValue={makeFinalValue(defaultValue)}
       onChangeText={handleChangeText}
     />
   );
 };
 
 export default NumberInput;
-
-// comment to try to fix sourcemap issue

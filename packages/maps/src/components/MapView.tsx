297809708e520769b74e70a2d8534b8bae4186eb
Donald Hruska <donaldhruska@gmail.com>
2022-04-28 20:02:42 -0700

    Support animating to a location in MapView (#446)
    * Support animating to a location in MapView
    
    * zoom to point if default location changes

--- packages/maps/src/components/MapView.tsx
+++ packages/maps/src/components/MapView.tsx
@@ -17,52 +17,99 @@ function zoomToAltitude(zoom: number) {
   return C * Math.pow((A - D) / (zoom - D) - 1, 1 / B);
 }
 
-const MapView: React.FC<MapViewProps> = ({
-  provider,
-  latitude,
-  longitude,
-  zoom,
-  showsCompass = false,
-  rotateEnabled = true,
-  zoomEnabled = true,
-  loadingEnabled = true,
-  scrollEnabled = true,
-  loadingBackgroundColor,
-  loadingIndicatorColor,
-  mapType = "standard",
-  style,
-  children,
-}) => {
-  if (!NativeMapView) {
-    return null;
+class MapView extends React.Component<MapViewProps> {
+  private mapRef: React.RefObject<any>;
+  constructor(props: MapViewProps) {
+    super(props);
+    this.mapRef = React.createRef();
   }
 
-  return (
-    <NativeMapView
-      provider={provider}
-      mapType={mapType}
-      showsCompass={showsCompass}
-      rotateEnabled={rotateEnabled}
-      zoomEnabled={zoomEnabled}
-      initialCamera={{
-        altitude: zoomToAltitude(zoom || 1),
-        heading: 0,
-        pitch: 0,
-        zoom,
-        center: {
-          latitude,
-          longitude,
-        },
-      }}
-      loadingEnabled={loadingEnabled}
-      scrollEnabled={scrollEnabled}
-      loadingBackgroundColor={loadingBackgroundColor}
-      loadingIndicatorColor={loadingIndicatorColor}
-      style={style}
-    >
-      {children}
-    </NativeMapView>
-  );
-};
+  componentDidUpdate(prevProps: MapViewProps) {
+    if (
+      prevProps.latitude != null &&
+      prevProps.longitude != null &&
+      this.props.latitude != null &&
+      this.props.longitude != null &&
+      (prevProps.latitude !== this.props.latitude ||
+        prevProps.longitude !== this.props.longitude)
+    ) {
+      this.animateToLocation({
+        latitude: this.props.latitude,
+        longitude: this.props.longitude,
+        zoom: this.props.zoom,
+      });
+    }
+  }
+
+  animateToLocation({
+    latitude,
+    longitude,
+    zoom,
+  }: {
+    latitude: number;
+    longitude: number;
+    zoom?: number;
+  }) {
+    this.mapRef.current.animateCamera({
+      altitude: zoomToAltitude(zoom || 1),
+      zoom,
+      center: {
+        latitude,
+        longitude,
+      },
+    });
+  }
+
+  render() {
+    const {
+      provider,
+      latitude,
+      longitude,
+      zoom,
+      showsCompass = false,
+      rotateEnabled = true,
+      zoomEnabled = true,
+      loadingEnabled = true,
+      scrollEnabled = true,
+      loadingBackgroundColor,
+      loadingIndicatorColor,
+      mapType = "standard",
+      style,
+      children,
+    } = this.props;
+
+    if (!NativeMapView) {
+      return null;
+    }
+
+    return (
+      <NativeMapView
+        ref={this.mapRef}
+        provider={provider}
+        mapType={mapType}
+        showsCompass={showsCompass}
+        rotateEnabled={rotateEnabled}
+        zoomEnabled={zoomEnabled}
+        initialCamera={{
+          altitude: zoomToAltitude(zoom || 1),
+          heading: 0,
+          pitch: 0,
+          zoom,
+          center: {
+            latitude,
+            longitude,
+          },
+        }}
+        loadingEnabled={loadingEnabled}
+        scrollEnabled={scrollEnabled}
+        loadingBackgroundColor={loadingBackgroundColor}
+        loadingIndicatorColor={loadingIndicatorColor}
+        style={style}
+      >
+        {children}
+      </NativeMapView>
+    );
+  }
+}
 
 export default MapView;
--- packages/web-maps/src/components/MapView.tsx
+++ packages/web-maps/src/components/MapView.tsx
@@ -4,44 +4,91 @@ import NoApiKey from "./NoApiKey";
 import { MapViewProps } from "@draftbit/types";
 import { StyleSheet } from "react-native";
 
-const MapView: React.FC<MapViewProps> = ({
-  apiKey,
-  latitude,
-  longitude,
-  zoom,
-  rotateEnabled = true,
-  scrollEnabled = true,
-  mapType = "standard",
-  style,
-  children,
-}) => {
-  if (!LoadScript || !GoogleMap) {
-    return null;
-  }
-
-  if (!apiKey) {
-    return <NoApiKey />;
-  }
-
-  return (
-    <LoadScript googleMapsApiKey={apiKey}>
-      <GoogleMap
-        mapContainerStyle={StyleSheet.flatten(style) as React.CSSProperties}
-        center={{
-          lat: latitude || 0,
-          lng: longitude || 0,
-        }}
-        mapTypeId={mapType}
-        zoom={zoom}
-        options={{
-          scrollwheel: scrollEnabled,
-          rotateControl: rotateEnabled,
-        }}
-      >
-        {children}
-      </GoogleMap>
-    </LoadScript>
-  );
+type State = {
+  lat: number;
+  lng: number;
+  zoom?: number;
 };
+
+class MapView extends React.Component<MapViewProps, State> {
+  constructor(props: MapViewProps) {
+    super(props);
+    this.state = {
+      lat: props.latitude || 0,
+      lng: props.longitude || 0,
+      zoom: props.zoom,
+    };
+  }
+
+  componentDidUpdate(prevProps: MapViewProps) {
+    if (
+      prevProps.latitude != null &&
+      prevProps.longitude != null &&
+      this.props.latitude != null &&
+      this.props.longitude != null &&
+      (prevProps.latitude !== this.props.latitude ||
+        prevProps.longitude !== this.props.longitude)
+    ) {
+      this.setState({
+        lat: this.props.latitude,
+        lng: this.props.longitude,
+        zoom: this.props.zoom,
+      });
+    }
+  }
+
+  animateToLocation({
+    latitude,
+    longitude,
+    zoom,
+  }: {
+    latitude: number;
+    longitude: number;
+    zoom?: number;
+  }) {
+    this.setState({ lat: latitude, lng: longitude, zoom });
+  }
+
+  render() {
+    const {
+      apiKey,
+      rotateEnabled = true,
+      scrollEnabled = true,
+      mapType = "standard",
+      style,
+      children,
+    } = this.props;
+
+    const { lat, lng, zoom } = this.state;
+
+    if (!LoadScript || !GoogleMap) {
+      return null;
+    }
+
+    if (!apiKey) {
+      return <NoApiKey />;
+    }
+
+    return (
+      <LoadScript googleMapsApiKey={apiKey}>
+        <GoogleMap
+          mapContainerStyle={StyleSheet.flatten(style) as React.CSSProperties}
+          center={{
+            lat,
+            lng,
+          }}
+          mapTypeId={mapType}
+          zoom={zoom}
+          options={{
+            scrollwheel: scrollEnabled,
+            rotateControl: rotateEnabled,
+          }}
+        >
+          {children}
+        </GoogleMap>
+      </LoadScript>
+    );
+  }
+}
 
 export default MapView;

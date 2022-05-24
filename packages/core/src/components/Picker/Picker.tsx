81d33dddbc27ef688f2c030a9672b7e496a9942b
Shawn Michael Larkin <shawn.michael.larkin@gmail.com>
2022-05-23 18:11:17 -0700

    Test color

--- packages/core/src/components/Picker/Picker.tsx
+++ packages/core/src/components/Picker/Picker.tsx
@@ -154,7 +154,7 @@ const Picker: React.FC<PickerProps> = ({
           }
         : {}),
       borderBottomWidth: 1,
-      borderColor: unstyledColor,
+      borderColor: "red",
       borderStyle: "solid",
     },
     ...extractedBorderStyles,

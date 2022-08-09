82a27c6531dfe520b2958970725ceed9fd088f1d
Brian Luerssen <bluerssen@users.noreply.github.com>
2022-08-08 21:59:13 -0500

    Fixes

--- packages/core/src/components/SVG.tsx
+++ packages/core/src/components/SVG.tsx
@@ -1,28 +1,22 @@
 import React from "react";
+import { View, ImageProps, ImageSourcePropType } from "react-native";
 import { SvgUri } from "react-native-svg";
 
 import Config from "./Config";
 
-const Svg: React.FC<ImageProps> = ({
-  source,
-  style,
-  ...props
-}) => {
+const Svg: React.FC<ImageProps> = ({ source, style }) => {
   let svgSource =
-    source === null || source === undefined
-      ? Config.placeholderSvgURL
-      : source;
+    source === null || source === undefined ? Config.placeholderSvgURL : source;
 
   return (
     <View style={style}>
       <SvgUri
         width="100%"
         height="100%"
-        uri={source as ImageSourcePropType}
+        uri={svgSource as ImageSourcePropType}
       />
-  </View>
-
+    </View>
   );
 };
 
-export default Image;
+export default Svg;

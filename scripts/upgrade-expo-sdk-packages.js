5b390dcfc88da8d862b094e8b08bb609fddd29c4
Peter Piekarczyk <peterpiekarczyk@gmail.com>
2022-08-23 08:16:18 -0500

    upgrade the other deps and add script

---
+++ scripts/upgrade-expo-sdk-packages.js
@@ -0,0 +1,58 @@
+const fs = require("fs/promises");
+const fetch = require("node-fetch");
+
+async function fetchBundledNativeModules(version) {
+  const json = await fetch(
+    `https://raw.githubusercontent.com/expo/expo/sdk-${version}/packages/expo/bundledNativeModules.json`
+  ).then((r) => r.json());
+  return json;
+}
+
+function upgradeDependencies(nativeModules, packageJson) {
+  const deps = packageJson.dependencies;
+  const devDeps = packageJson.devDependencies;
+
+  for (const key in deps) {
+    let value = deps[key];
+
+    if (nativeModules.hasOwnProperty(key)) {
+      console.log("has key", key, nativeModules[key], deps[key]);
+      packageJson.dependencies[key] = nativeModules[key];
+    }
+  }
+
+  for (const key in devDeps) {
+    let value = devDeps[key];
+
+    if (nativeModules.hasOwnProperty(key)) {
+      console.log("has key", key, nativeModules[key], deps[key]);
+      packageJson.devDependencies[key] = nativeModules[key];
+    }
+  }
+
+  return packageJson;
+}
+
+const main = async () => {
+  const expoSdkModules = await fetchBundledNativeModules("46");
+  const folders = [
+    "example",
+    "packages/core",
+    "packages/maps",
+    "packages/native",
+    "packages/types",
+    "packages/ui",
+    "packages/web-maps",
+  ];
+
+  for (const folder of folders) {
+    const packageJsonPath = `./${folder}/package.json`;
+    const json = JSON.parse(await fs.readFile(packageJsonPath, "utf-8"));
+    const updatedJson = upgradeDependencies(expoSdkModules, json);
+    await fs.writeFile(packageJsonPath, JSON.stringify(updatedJson, null, 2));
+  }
+
+  console.log("Done. Run yarn");
+};
+
+main();

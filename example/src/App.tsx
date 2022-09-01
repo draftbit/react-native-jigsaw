d23a462952446249528c03ce91d5384fc6aaf6e4
Peter Piekarczyk <peterpme@users.noreply.github.com>
2022-09-01 07:45:21 -0500

    AudioPlayerExample (#504)
    * Update AudioPlayer.tsx to handle audio finished
    
    This change should reset the audio player after the source has finished playing.
    
    In this context, reset means:
    - Unload Audio
    - Set slider position to zero
    - Show the play icon
    
    * adds audioplayer example
    
    * updates
    
    * adds example
    
    Co-authored-by: Willie Williams <bigwilliestyle@gmail.com>

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -15,6 +15,7 @@ import {
 import AppLoading from "expo-app-loading";
 import * as Font from "expo-font";
 
+import AudioPlayerExample from "./AudioPlayerExample";
 import CircleImageExample from "./CircleImageExample";
 import IconExample from "./IconExample";
 import BannerExample from "./BannerExample";
@@ -70,6 +71,7 @@ import LinearGradientExample from "./LinearGradientExample";
 import SurfaceExample from "./SurfaceExample";
 
 const ROUTES = {
+  AudioPlayer: AudioPlayerExample,
   Layout: LayoutExample,
   Icon: IconExample,
   Banner: BannerExample,

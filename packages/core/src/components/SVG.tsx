dbff94b7b7559086319cebd061d956ef9d6c9e30
Brian Luerssen <brian@draftbit.com>
2022-09-16 13:11:39 -0500

    Update SVG.tsx

--- packages/core/src/components/SVG.tsx
+++ packages/core/src/components/SVG.tsx
@@ -1,39 +1,32 @@
 import * as React from "react";
-import {
-  View,
-  Platform,
-  Image,
-  StyleProp,
-  ImageStyle,
-  ImageSourcePropType,
-} from "react-native";
+import { View, Platform, Image, StyleProp, ImageStyle } from "react-native";
 import { SvgUri } from "react-native-svg";
 
 import Config from "./Config";
 
 type SvgComponentProps = {
-  source: string | ImageSourcePropType;
+  source: string;
   style?: StyleProp<ImageStyle>;
 };
 
-const Svg = ({ source, style }: SvgComponentProps) => {
-  let svgSource =
-    source === null || source === undefined ? Config.placeholderSvgURL : source;
-
+const Svg: React.FC<React.PropsWithChildren<Props>> = ({
+  source = Config.placeholderSvgURL,
+  style,
+}) => {
   return (
     <>
       {Platform.OS === "ios" && (
         <View style={style}>
-          <SvgUri width="100%" height="100%" uri={svgSource} />
+          <SvgUri width="100%" height="100%" uri={source} />
         </View>
       )}
       {Platform.OS === "android" && (
         <View style={style}>
-          <SvgUri width="100%" height="100%" uri={svgSource} />
+          <SvgUri width="100%" height="100%" uri={source} />
         </View>
       )}
       {Platform.OS === "web" && (
-        <Image style={style} source={{ uri: svgSource }} />
+        <Image style={style} source={{ uri: source }} />
       )}
     </>
   );

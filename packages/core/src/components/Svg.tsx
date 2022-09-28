23921e4c13d836c774bbdc015313f689f1442c08
Brian Luerssen <brian@draftbit.com>
2022-09-27 20:13:24 -0500

    ImageStyle

--- packages/core/src/components/Svg.tsx
+++ packages/core/src/components/Svg.tsx
@@ -1,19 +1,12 @@
 import * as React from "react";
-import {
-  View,
-  Platform,
-  Image,
-  StyleProp,
-  ImageStyle,
-  ViewStyle,
-} from "react-native";
+import { View, Platform, Image, StyleProp, ImageStyle } from "react-native";
 import { SvgUri } from "react-native-svg";
 
 import Config from "./Config";
 
 type SvgComponentProps = {
   source: string;
-  style?: StyleProp<ViewStyle | ImageStyle>;
+  style?: StyleProp<ImageStyle>;
 };
 
 const Svg: React.FC<React.PropsWithChildren<SvgComponentProps>> = ({

5e097b6f58076f4287a84511edff7d30e04cb36a
Gunjan Kothari <gunjan.kothari2002@gmail.com>
2022-09-29 23:06:08 +0530

    Table Components renamed files

--- packages/core/src/components/Table/index.tsx
+++ packages/core/src/components/Table/index.tsx
@@ -1,6 +1,6 @@
-export { default as Table } from "./table";
-export { default as TableHeader } from "./tableHeader";
-export { default as TablePaginator } from "./tablePaginator";
-export { default as TableRow } from "./tableRow";
-export { default as TableCell } from "./tableCell";
-export { default as TableTitle } from "./tableTitle";
+export { default as Table } from "./Table";
+export { default as TableHeader } from "./TableHeader";
+export { default as TablePaginator } from "./TablePaginator";
+export { default as TableRow } from "./TableRow";
+export { default as TableCell } from "./TableCell";
+export { default as TableTitle } from "./TableTitle";

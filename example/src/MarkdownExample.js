57db5ede93d98c99a4c034d31ca50abef8486e81
Gunjan Kothari <gunjan.kothari2002@gmail.com>
2022-10-01 12:58:05 +0530

    P-2565 Markdown Component

--- example/src/MarkdownExample.js
+++ example/src/MarkdownExample.js
@@ -5,11 +5,17 @@ import Section, { Container } from "./Section";
 const copy = `
 ---
 # h1 Heading 8-)
+
 ## h2 Heading
+
 ### h3 Heading
+
 #### h4 Heading
+
 ##### h5 Heading
+
 ###### h6 Heading
+
 ## Horizontal Rules
 ___
 ---
--- packages/core/src/components/Markdown.tsx
+++ packages/core/src/components/Markdown.tsx
@@ -12,59 +12,14 @@ const MarkdownComponent: React.FC<Props> = ({ content }) => {
       {
         // @ts-ignore
       }
-      <Markdown styles={markdownStyle.collectiveMd}>{content}</Markdown>
+      <Markdown styles={markdownStyle}>{content}</Markdown>
     </>
   );
 };
 
 const markdownStyle = {
-  singleLineMd: {
-    text: {
-      color: "blue",
-      textAlign: "right",
-    },
-    view: {
-      alignSelf: "stretch",
-    },
-  },
-  collectiveMd: {
-    heading1: {
-      color: "red",
-    },
-    heading2: {
-      color: "green",
-      textAlign: "right",
-    },
-    strong: {
-      color: "blue",
-    },
-    em: {
-      color: "cyan",
-    },
-    text: {
-      color: "black",
-    },
-    blockQuoteText: {
-      color: "grey",
-    },
-    blockQuoteSection: {
-      flexDirection: "row",
-    },
-    blockQuoteSectionBar: {
-      width: 3,
-      height: null,
-      backgroundColor: "#DDDDDD",
-      marginRight: 15,
-    },
-    codeBlock: {
-      fontFamily: "Courier",
-      fontWeight: "500",
-      backgroundColor: "#DDDDDD",
-    },
-    tableHeader: {
-      backgroundColor: "grey",
-    },
-  },
+  singleLineMd: {},
+  collectiveMd: {},
 };
 
 export default MarkdownComponent;

731f40760f01a0850fdfc614f0ae80a2b100ede2
Brian Luerssen <brian@draftbit.com>
2022-10-03 11:26:48 -0500

    Create FlashList.ts

---
+++ packages/core/src/mappings/FlashList.ts
@@ -0,0 +1,39 @@
+import {
+  COMPONENT_TYPES,
+  createNumColumnsType,
+  createStaticBoolProp,
+  createNumberProp,
+  CONTAINER_COMPONENT_STYLES_SECTIONS,
+} from "@draftbit/types";
+
+export const SEED_DATA = {
+  name: "FlashList",
+  tag: "FlashList",
+  description: "Flashlist by Shopify",
+  category: COMPONENT_TYPES.data,
+  stylesPanelSections: CONTAINER_COMPONENT_STYLES_SECTIONS,
+  layout: {
+    flex: 1,
+  },
+  props: {
+    estimatedItemSize: createNumberProp({
+      group: GROUPS.basic,
+      label: "Est. Item Size",
+      description: "Approximate size of the items before rendering.",
+      defaultValue: 50,
+      step: 1,
+      precision: 0,
+    }),
+    horizontal: createStaticBoolProp({
+      label: "Horizontal",
+      description: "Render list horizontally",
+    }),
+    inverted: createStaticBoolProp({
+      label: "Inverted",
+      description: "If true, reverses the direction.",
+    }),
+    numColumns: createNumColumnsType({
+      editable: true,
+    }),
+  },
+};

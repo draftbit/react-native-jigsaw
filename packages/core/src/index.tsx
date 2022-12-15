aebd7e6bc2c004d334f48ff3d07ed68902930a13
Brian Luerssen <brian@draftbit.com>
2022-12-15 14:14:33 -0600

    Attempt to remove Container & Disable Cache

--- packages/core/src/index.tsx
+++ packages/core/src/index.tsx
@@ -12,7 +12,6 @@ export { default as Card } from "./components/Card";
 export { default as Carousel } from "./components/Carousel";
 export { Checkbox, CheckboxGroup, CheckboxRow } from "./components/Checkbox";
 export { default as CircleImage } from "./components/CircleImage";
-export { default as Container } from "./components/Container";
 export { default as Divider } from "./components/Divider";
 export { default as FAB } from "./components/FAB";
 export { default as FieldSearchBarFull } from "./components/FieldSearchBarFull";

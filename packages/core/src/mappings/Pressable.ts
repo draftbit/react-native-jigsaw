533eae72c6fc6289d7549c9458085f7cc50535db
Brian Luerssen <brian@draftbit.com>
2022-12-23 14:11:51 -0500

    Update Pressable.ts

--- packages/core/src/mappings/Pressable.ts
+++ packages/core/src/mappings/Pressable.ts
@@ -17,10 +17,30 @@ const SEED_DATA_PROPS = {
   props: {
     onPress: createActionProp(),
     onLongPress: createActionProp(),
-    activeOpacity: createNumberProp(),
-    disabledOpacity: createNumberProp(),
-    delayLongPress: createNumberProp(),
-    hitSlop: createNumberProp(),
+    activeOpacity: createNumberProp({
+      label: "Active Opacity",
+      description: "The opacity when the button is pressed.",
+      defaultValue: 0.8,
+      group: GROUPS.basic,
+    }),
+    disabledOpacity: createNumberProp({
+      label: "Disabled Opacity",
+      description: "The opacity when the button is disabled.",
+      defaultValue: 0.8,
+      group: GROUPS.basic,
+    }),
+    delayLongPress: createNumberProp({
+      label: "Delay Long Press",
+      description:
+        "Duration (in milliseconds) from onPressIn before onLongPress is called.",
+      group: GROUPS.basic,
+    }),
+    hitSlop: createNumberProp({
+      label: "Hit Slop",
+      description:
+        "Sets additional distance outside of element in which a press can be detected.",
+      group: GROUPS.basic,
+    }),
   },
 };
 
@@ -29,14 +49,14 @@ export const SEED_DATA = [
     name: "Touchable",
     tag: "Touchable",
     description: "An interactive view with no styles",
-    category: COMPONENT_TYPES.deprecated,
+    category: COMPONENT_TYPES.button,
     ...SEED_DATA_PROPS,
   },
   {
     name: "Pressable",
     tag: "Pressable",
     description: "An interactive view with no styles",
-    category: COMPONENT_TYPES.button,
+    category: COMPONENT_TYPES.deprecated,
     ...SEED_DATA_PROPS,
   },
 ];

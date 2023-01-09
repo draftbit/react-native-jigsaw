b300822c344b81deda5c0c38fa2e881c1c34c3b0
Brian Luerssen <bluerssen@users.noreply.github.com>
2023-01-09 14:14:39 -0600

    Add Masonry List and Support for OnRefresh (#557)
    * Add Masonry List and Support for OnRefresh
    
    * Fix Triggers and Props
    
    * Oops
    
    * Update packages/types/src/component-types.ts
    
    Co-authored-by: Allen Nelson <adnelson@users.noreply.github.com>
    
    * Update packages/types/src/component-types.ts
    
    Co-authored-by: Allen Nelson <adnelson@users.noreply.github.com>
    
    Co-authored-by: Allen Nelson <adnelson@users.noreply.github.com>

--- packages/core/src/mappings/FlashList.ts
+++ packages/core/src/mappings/FlashList.ts
@@ -5,37 +5,89 @@ import {
   createNumberProp,
   CONTAINER_COMPONENT_STYLES_SECTIONS,
   GROUPS,
+  Triggers,
+  createActionProp,
+  createStaticNumberProp,
 } from "@draftbit/types";
 
-export const SEED_DATA = {
-  name: "FlashList",
-  tag: "FlashList",
-  description: "Flashlist by Shopify",
-  packageName: "@shopify/flash-list",
-  category: COMPONENT_TYPES.data,
-  stylesPanelSections: CONTAINER_COMPONENT_STYLES_SECTIONS,
-  layout: {
-    flex: 1,
+export const SEED_DATA = [
+  {
+    name: "Masonry List",
+    tag: "MasonryFlashList",
+    description: "Masonry Flashlist by Shopify",
+    packageName: "@shopify/flash-list",
+    category: COMPONENT_TYPES.data,
+    stylesPanelSections: CONTAINER_COMPONENT_STYLES_SECTIONS,
+    layout: {
+      flex: 1,
+    },
+    triggers: [Triggers.OnRefresh, Triggers.OnEndReached],
+    props: {
+      onRefresh: createActionProp(),
+      onEndReached: createActionProp(),
+      numColumns: createNumColumnsType({
+        editable: true,
+      }),
+      estimatedItemSize: createNumberProp({
+        group: GROUPS.basic,
+        label: "Est. Item Size",
+        description: "Approximate size of the items before rendering.",
+        defaultValue: 50,
+        step: 1,
+        precision: 0,
+      }),
+      optimizeItemArrangement: createStaticBoolProp({
+        label: "Optimize Item Arrangement",
+        description:
+          "If enabled, MasonryFlashList will try to reduce difference in column height by modifying item order. If true, specifying overrideItemLayout is required. Default value is false.",
+      }),
+      onEndReachedThreshold: createStaticNumberProp({
+        label: "End Reached Threshold",
+        description:
+          "How far from the end (in units of visible length of the list) the bottom edge of the list must be from the end of the content to trigger the onEndReached callback. Thus a value of 0.5 will trigger onEndReached when the end of the content is within half the visible length of the list.",
+        defaultValue: 0.5,
+      }),
+    },
   },
-  props: {
-    estimatedItemSize: createNumberProp({
-      group: GROUPS.basic,
-      label: "Est. Item Size",
-      description: "Approximate size of the items before rendering.",
-      defaultValue: 50,
-      step: 1,
-      precision: 0,
-    }),
-    horizontal: createStaticBoolProp({
-      label: "Horizontal",
-      description: "Render list horizontally",
-    }),
-    inverted: createStaticBoolProp({
-      label: "Inverted",
-      description: "If true, reverses the direction.",
-    }),
-    numColumns: createNumColumnsType({
-      editable: true,
-    }),
+  {
+    name: "FlashList",
+    tag: "FlashList",
+    description: "Flashlist by Shopify",
+    packageName: "@shopify/flash-list",
+    category: COMPONENT_TYPES.data,
+    stylesPanelSections: CONTAINER_COMPONENT_STYLES_SECTIONS,
+    layout: {
+      flex: 1,
+    },
+    triggers: [Triggers.OnRefresh, Triggers.OnEndReached],
+    props: {
+      onRefresh: createActionProp(),
+      onEndReached: createActionProp(),
+      estimatedItemSize: createNumberProp({
+        group: GROUPS.basic,
+        label: "Est. Item Size",
+        description: "Approximate size of the items before rendering.",
+        defaultValue: 50,
+        step: 1,
+        precision: 0,
+      }),
+      horizontal: createStaticBoolProp({
+        label: "Horizontal",
+        description: "Render list horizontally",
+      }),
+      inverted: createStaticBoolProp({
+        label: "Inverted",
+        description: "If true, reverses the direction.",
+      }),
+      numColumns: createNumColumnsType({
+        editable: true,
+      }),
+      onEndReachedThreshold: createStaticNumberProp({
+        label: "End Reached Threshold",
+        description:
+          "How far from the end (in units of visible length of the list) the bottom edge of the list must be from the end of the content to trigger the onEndReached callback. Thus a value of 0.5 will trigger onEndReached when the end of the content is within half the visible length of the list.",
+        defaultValue: 0.5,
+      }),
+    },
   },
-};
+];

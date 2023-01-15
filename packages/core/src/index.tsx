0b646ce0e7317fecdde339530d63aad0f0f21c21
Brian Luerssen <brian@draftbit.com>
2023-01-15 13:27:00 -0600

    Remove Components and Mappings We Dont Need

--- packages/core/src/index.tsx
+++ packages/core/src/index.tsx
@@ -3,40 +3,11 @@ export { withTheme, ThemeProvider } from "./theming";
 export { default as Provider } from "./Provider";
 export { default as DefaultTheme } from "./styles/DefaultTheme";
 
-export { Link } from "./components/Text";
-export { default as Banner } from "./components/Banner";
-export { Button, ButtonSolid, ButtonOutline } from "./components/Button";
-export { default as Avatar } from "./components/CircleImage";
-export { default as AvatarEdit } from "./components/AvatarEdit";
-export { default as Card } from "./components/Card";
-export { default as Carousel } from "./components/Carousel";
-export { Checkbox, CheckboxGroup, CheckboxRow } from "./components/Checkbox";
-export { default as CircleImage } from "./components/CircleImage";
-export { default as Container } from "./components/Container";
-export { default as Divider } from "./components/Divider";
-export { default as FAB } from "./components/FAB";
-export { default as FieldSearchBarFull } from "./components/FieldSearchBarFull";
-export { default as IconButton } from "./components/IconButton";
-export { default as Image } from "./components/Image";
-export { default as SVG } from "./components/SVG";
-export { default as NumberInput } from "./components/NumberInput";
-export { default as ScreenContainer } from "./components/ScreenContainer";
-export { default as StarRating } from "./components/StarRating";
-export { default as Surface } from "./components/Surface";
-export { default as Switch, SwitchRow } from "./components/Switch";
-export { default as TextField } from "./components/TextField";
-export { default as ToggleButton } from "./components/ToggleButton";
-export { default as Touchable } from "./components/Touchable";
-export { default as Pressable } from "./components/Pressable";
-export { AccordionGroup, AccordionItem } from "./components/Accordion";
-export {
-  ActionSheet,
-  ActionSheetItem,
-  ActionSheetCancel,
-} from "./components/ActionSheet";
-export { Swiper, SwiperItem } from "./components/Swiper";
-
-export {
+/** Replaced By NativeBase
+ * export { default as Avatar } from "./components/CircleImage";
+ * export { default as Container } from "./components/Container";
+ * export { default as FAB } from "./components/FAB";
+ * export {
   Center,
   Circle,
   Square,
@@ -44,31 +15,127 @@ export {
   Stack,
   Spacer,
 } from "./components/Layout";
+ */
 
+// BASIC
+export { default as ScreenContainer } from "./components/ScreenContainer";
+export { default as Banner } from "./components/Banner";
+
+// BUTTONS
+export { Button, ButtonSolid, ButtonOutline } from "./components/Button";
+export { Link } from "./components/Text";
+export { default as IconButton } from "./components/IconButton";
+export { default as Touchable } from "./components/Touchable";
+export { default as ToggleButton } from "./components/ToggleButton";
+
+// INPUTS
+export { default as DatePicker } from "./components/DatePicker/DatePicker";
+export { default as NumberInput } from "./components/NumberInput";
+export { default as TextField } from "./components/TextField";
+export { default as FieldSearchBarFull } from "./components/FieldSearchBarFull";
+export { default as Picker } from "./components/Picker/Picker";
+
+// CONTROLS
+export { Checkbox, CheckboxGroup, CheckboxRow } from "./components/Checkbox";
 export {
   RadioButton,
   RadioButtonGroup,
   RadioButtonRow,
   RadioButtonFieldGroup,
 } from "./components/RadioButton/index";
-
-/* Deprecated: Fix or Delete!  */
-export { default as CardBlock } from "./components/CardBlock";
-export { default as CardContainer } from "./components/CardContainer";
-export { default as CardContainerRating } from "./components/CardContainerRating";
-export { default as CardInline } from "./components/CardInline";
-export { default as DatePicker } from "./components/DatePicker/DatePicker";
-export { default as HeaderLarge } from "./components/HeaderLarge";
-export { default as HeaderMedium } from "./components/HeaderMedium";
-export { default as HeaderOverline } from "./components/HeaderOverline";
-export { default as Picker } from "./components/Picker/Picker";
-export { default as ProgressBar } from "./components/ProgressBar";
-export { default as ProgressCircle } from "./components/ProgressCircle";
-export { default as RowBodyIcon } from "./components/RowBodyIcon";
-export { default as RowHeadlineImageCaption } from "./components/RowHeadlineImageCaption";
-export { default as RowHeadlineImageIcon } from "./components/RowHeadlineImageIcon";
 export { default as Slider } from "./components/Slider";
 export { default as Stepper } from "./components/Stepper";
+export { default as StarRating } from "./components/StarRating";
+export { default as Switch, SwitchRow } from "./components/Switch";
+
+// MEDIA
+export { default as SVG } from "./components/SVG";
+export { default as Image } from "./components/Image";
+// export { default as Avatar } from "./components/CircleImage"; Replaced by NativeBase
+export { default as AvatarEdit } from "./components/AvatarEdit";
+export { default as CircleImage } from "./components/CircleImage";
+
+// SWIPER
+export { Swiper, SwiperItem } from "./components/Swiper";
+
+// CONTAINERS
+export { AccordionGroup, AccordionItem } from "./components/Accordion";
+export { default as Surface } from "./components/Surface";
+
+// ACTIONSHEET
+export {
+  ActionSheet,
+  ActionSheetItem,
+  ActionSheetCancel,
+} from "./components/ActionSheet";
+
+// OTHER
+export { default as Divider } from "./components/Divider";
+export { default as ProgressBar } from "./components/ProgressBar";
+export { default as ProgressCircle } from "./components/ProgressCircle";
 export { useAuthState } from "./components/useAuthState";
 
-// a comment to fix sourcemap comment issue
+/**
+ * NativeBase Components Jan 2023
+ *
+ * ALERT
+ * - AlertDialog
+ * - AlertDialog.Header
+ * - AlertDialog.Body
+ * - AlertDialog.Footer
+ * - AlertDialog.Content
+ * - AlertDialog.CloseButton
+ *
+ * FORMS
+ * - Button -- COMMENTED OUT
+ * - Button.Group -- COMMENTED OUT
+ * - Fab
+ *
+ * DATA DISPLAY
+ * - Badge
+ * - Divider
+ *
+ * FEEDBACK
+ * - Alert
+ * - Alert.Icon
+ * - Progress
+ *
+ * LAYOUT
+ * - AspectRatio
+ * - Box
+ * - Center
+ * - Square
+ * - Circle
+ * - Column
+ * - Container
+ * - Flex
+ * - Spacer
+ * - Row
+ * - Stack
+ * - ZStack
+ *
+ * MEDIA
+ * - Avatar
+ * - Avatar.Badge
+ * - Avatar.Group
+ *
+ * MENU
+ * - Menu
+ * - Menu.Item
+ * - Menu.Group
+ * - Menu.OptionGroup
+ * - Menu.ItemOption
+ * - Menu.Trigger
+ *
+ * MODAL
+ * - Modal
+ * - Modal.Content
+ * - Modal.Header
+ * - Modal.Footer
+ * - Modal.Body
+ * - Modal.CloseButton
+ *
+ * OTHER
+ * - Tooltip
+ *
+ * */

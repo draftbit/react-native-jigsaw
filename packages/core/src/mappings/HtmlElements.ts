1fa7e645d105728d25b751f2348ffb6b01b446ad
Youssef Henna <youssef.hisham14@gmail.com>
2023-02-19 15:21:05 +0200

    Updated mapping with more components and fixed props

--- packages/core/src/mappings/HtmlElements.ts
+++ packages/core/src/mappings/HtmlElements.ts
@@ -5,12 +5,20 @@ import {
   PROP_TYPES,
   GROUPS,
   StylesPanelSections,
+  BLOCK_STYLES_SECTIONS,
 } from "@draftbit/types";
 
 const ELEMENT_SEED_DATA = {
   doc_link: "https://www.npmjs.com/package/@expo/html-elements",
   code_link: "https://github.com/expo/expo/tree/master/packages/html-elements",
   packageName: "@expo/html-elements",
+  stylesPanelSections: BLOCK_STYLES_SECTIONS,
+  category: COMPONENT_TYPES.webelement,
+};
+
+const TEXT_SEED_DATA = {
+  ...ELEMENT_SEED_DATA,
+  category: COMPONENT_TYPES.text,
   stylesPanelSections: [
     StylesPanelSections.Typography,
     StylesPanelSections.LayoutSelectedItem,
@@ -18,15 +26,19 @@ const ELEMENT_SEED_DATA = {
     StylesPanelSections.Effects,
   ],
   layout: {
-    margin: 0,
+    color: "strong",
   },
-  category: COMPONENT_TYPES.webelement,
-};
-
-const HEADING_SEED_DATA = {
-  ...ELEMENT_SEED_DATA,
-  category: COMPONENT_TYPES.text,
   props: {
+    children: {
+      group: GROUPS.data,
+      label: "Text",
+      description: "Text",
+      defaultValue: "Double click me to edit 👀",
+      editable: true,
+      required: true,
+      formType: FORM_TYPES.string,
+      propType: PROP_TYPES.STRING,
+    },
     accessibilityLabel: {
       group: GROUPS.accessibility,
       name: "accessibilityLabel",
@@ -49,74 +61,119 @@ const HEADING_SEED_DATA = {
       required: false,
       formType: FORM_TYPES.boolean,
       propType: PROP_TYPES.BOOLEAN,
-      defaultValue: null,
-    },
-    children: {
-      group: GROUPS.data,
-      label: "Text",
-      description: "Text",
-      editable: true,
-      required: true,
-      formType: FORM_TYPES.string,
-      propType: PROP_TYPES.STRING,
-      defaultValue: "Your Headline Here",
-    },
-  },
-};
-
-const TEXT_SEED_DATA = {
-  ...ELEMENT_SEED_DATA,
-  category: COMPONENT_TYPES.text,
-  props: {
-    children: {
-      group: GROUPS.data,
-      label: "Text",
-      description: "Text",
-      defaultValue: "Your Text Here",
-      editable: true,
-      required: true,
-      formType: FORM_TYPES.string,
-      propType: PROP_TYPES.STRING,
+      defaultValue: true,
     },
   },
 };
 
 export const SEED_DATA = [
   {
-    name: "H1",
+    name: "Heading 1",
     tag: "H1",
-    ...HEADING_SEED_DATA,
+    ...TEXT_SEED_DATA,
   },
   {
-    name: "H2",
+    name: "Heading 2",
     tag: "H2",
-    ...HEADING_SEED_DATA,
+    ...TEXT_SEED_DATA,
   },
   {
-    name: "H3",
+    name: "Heading 3",
     tag: "H3",
-    ...HEADING_SEED_DATA,
+    ...TEXT_SEED_DATA,
   },
   {
-    name: "H4",
+    name: "Heading 4",
     tag: "H4",
-    ...HEADING_SEED_DATA,
+    ...TEXT_SEED_DATA,
   },
   {
-    name: "H5",
+    name: "Heading 5",
     tag: "H5",
-    ...HEADING_SEED_DATA,
+    ...TEXT_SEED_DATA,
   },
   {
-    name: "H6",
+    name: "Heading 6",
     tag: "H6",
-    ...HEADING_SEED_DATA,
+    ...TEXT_SEED_DATA,
+  },
+  {
+    name: "Paragraph",
+    tag: "P",
+    ...TEXT_SEED_DATA,
+  },
+  {
+    name: "Bold",
+    tag: "B",
+    ...TEXT_SEED_DATA,
+  },
+  {
+    name: "Strikethrough",
+    tag: "S",
+    ...TEXT_SEED_DATA,
+  },
+  {
+    name: "Italic",
+    tag: "I",
+    ...TEXT_SEED_DATA,
+  },
+  {
+    name: "Code",
+    tag: "Code",
+    ...TEXT_SEED_DATA,
+  },
+  {
+    name: "Preformatted",
+    tag: "Pre",
+    ...TEXT_SEED_DATA,
+  },
+  {
+    name: "Mark",
+    tag: "Mark",
+    ...TEXT_SEED_DATA,
+  },
+  {
+    name: "Block Quote",
+    tag: "BlockQuote",
+    ...TEXT_SEED_DATA,
+  },
+  {
+    name: "Quoted",
+    tag: "Q",
+    ...TEXT_SEED_DATA,
+  },
+  {
+    name: "Line Break",
+    tag: "BR",
+    ...TEXT_SEED_DATA,
+  },
+  {
+    name: "Horizontal Rule",
+    tag: "HR",
+    ...TEXT_SEED_DATA,
+  },
+  {
+    name: "Time",
+    tag: "Time",
+    ...TEXT_SEED_DATA,
+  },
+  {
+    name: "Unordered List",
+    tag: "UL",
+    ...ELEMENT_SEED_DATA,
+  },
+  {
+    name: "List Item",
+    tag: "LI",
+    ...TEXT_SEED_DATA,
+    category: COMPONENT_TYPES.webelement,
   },
   {
     name: "Anchor",
     tag: "A",
     ...TEXT_SEED_DATA,
     props: {
+      ...TEXT_SEED_DATA.props,
       href: createTextProp({
         label: "href",
         description: "Specify the URL",
@@ -131,74 +188,6 @@ export const SEED_DATA = [
         options: ["_blank", "_self", "_parent", "_top"],
       },
     },
-  },
-  {
-    name: "Paragraph",
-    tag: "P",
-    ...TEXT_SEED_DATA,
-    props: {
-      // NOTE - Keeping the props commented until we figure out a way to use this props from draftbit side.
-      // strong: createBoolProp({
-      //     label: "Strong",
-      //     description: "Strong",
-      // }),
-      // strike: createBoolProp({
-      //     label: "Strike",
-      //     description: "Strike through",
-      // }),
-      // italic: createBoolProp({
-      //     label: "Italic",
-      //     description: "Italic Fonts",
-      // }),
-      // bold: createBoolProp({
-      //     label: "Bold",
-      //     description: "Bold",
-      // }),
-    },
-  },
-  {
-    name: "Code",
-    tag: "Code",
-    ...TEXT_SEED_DATA,
-  },
-  {
-    name: "Pre",
-    tag: "Pre",
-    ...TEXT_SEED_DATA,
-  },
-  {
-    name: "Mark",
-    tag: "Mark",
-    ...TEXT_SEED_DATA,
-  },
-  {
-    name: "BR",
-    tag: "BR",
-    ...TEXT_SEED_DATA,
-  },
-  {
-    name: "BlockQuote",
-    tag: "BlockQuote",
-    ...TEXT_SEED_DATA,
-  },
-  {
-    name: "HR",
-    tag: "HR",
-    ...TEXT_SEED_DATA,
-  },
-  {
-    name: "Time",
-    tag: "Time",
-    ...ELEMENT_SEED_DATA,
-  },
-  {
-    name: "UL",
-    tag: "UL",
-    ...ELEMENT_SEED_DATA,
-  },
-  {
-    name: "LI",
-    tag: "LI",
-    ...ELEMENT_SEED_DATA,
+    category: COMPONENT_TYPES.webelement,
   },
 ];

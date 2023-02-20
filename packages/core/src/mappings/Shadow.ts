17d9fdc37f043cb732aff62fcc81b801fe5c2805
Youssef Henna <youssef.hisham14@gmail.com>
2023-02-20 12:20:40 +0200

    Updated mapping to include advanced props

--- packages/core/src/mappings/Shadow.ts
+++ packages/core/src/mappings/Shadow.ts
@@ -5,6 +5,7 @@ import {
   createStaticNumberProp,
   createStaticBoolProp,
   createDisabledProp,
+  GROUPS,
 } from "@draftbit/types";
 
 export const SEED_DATA = {
@@ -55,41 +56,49 @@ export const SEED_DATA = {
       label: "Show Shadow Start",
       description: "Whether to show shadow on the start side or not",
       defaultValue: true,
+      group: GROUPS.advanced,
     }),
     showShadowSideEnd: createStaticBoolProp({
       label: "Show Shadow End",
       description: "Whether to show shadow on the end side or not",
       defaultValue: true,
+      group: GROUPS.advanced,
     }),
     showShadowSideTop: createStaticBoolProp({
       label: "Show Shadow Top",
       description: "Whether to show shadow on the top side or not",
       defaultValue: true,
+      group: GROUPS.advanced,
     }),
     showShadowSideBottom: createStaticBoolProp({
       label: "Show Shadow Bottom",
       description: "Whether to show shadow on the bottom side or not",
       defaultValue: true,
+      group: GROUPS.advanced,
     }),
     showShadowCornerTopStart: createStaticBoolProp({
       label: "Show Shadow Top Start Corner",
       description: "Whether to show shadow on the top start corner or not",
       defaultValue: true,
+      group: GROUPS.advanced,
     }),
     showShadowCornerTopEnd: createStaticBoolProp({
       label: "Show Shadow Top End Corner",
       description: "Whether to show shadow on the top end corner or not",
       defaultValue: true,
+      group: GROUPS.advanced,
     }),
     showShadowCornerBottomStart: createStaticBoolProp({
       label: "Show Shadow Bottom Start Corner",
       description: "Whether to show shadow on the bottom start corner or not",
       defaultValue: true,
+      group: GROUPS.advanced,
     }),
     showShadowCornerBottomEnd: createStaticBoolProp({
       label: "Show Shadow Bottom End Corner",
       description: "Whether to show shadow on the bottom end corner or not",
       defaultValue: true,
+      group: GROUPS.advanced,
     }),
   },
 };

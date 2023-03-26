0baa4ccf02e8cf6262f05838ec5ba81a34774e73
Youssef Henna <youssef.hisham14@gmail.com>
2023-03-26 11:58:23 +0200

    Remove mappings and collector script

--- packages/core/src/mappings/CustomCode.ts
+++
@@ -1,9 +0,0 @@
-import { COMPONENT_TYPES } from "@draftbit/types";
-
-export const SEED_DATA = {
-  name: "Custom Code",
-  tag: "CustomCode",
-  description: "A component that allows usage of custom code",
-  category: COMPONENT_TYPES.utility,
-  props: {},
-};
--- scripts/paths.js
+++ scripts/paths.js
@@ -4,16 +4,7 @@ const NATIVE_ROOT = path.join(__dirname, "..", "packages", "native", "src");
 const NATIVE_COMPONENTS = path.join(NATIVE_ROOT, "components");
 const CORE_PATH = path.join(__dirname, "..", "packages", "core", "src");
 const COMPONENT_PATH = path.join(CORE_PATH, "components");
-const MAPPING_PATH = path.join(CORE_PATH, "mappings");
 const SCREEN_PATH = path.join(CORE_PATH, "screens");
-const COMPONENT_TYPES_PATH = path.join(
-  __dirname,
-  "..",
-  "packages",
-  "types",
-  "src",
-  "component-types.ts"
-);
 
 const EXAMPLE_ROOT = path.join(__dirname, "..", "example");
 

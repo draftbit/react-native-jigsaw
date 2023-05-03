f662022fe60165e8fedda33ef32fd6b74ae3bf44
Youssef Henna <youssef.hisham14@gmail.com>
2023-05-03 12:31:08 +0300

    Updated MapView to use @teovilla/react-native-web-maps

--- packages/maps/src/components/ReactNativeMaps.tsx
+++ packages/maps/src/components/ReactNativeMaps.tsx
@@ -1,8 +1,13 @@
 import { Platform } from "react-native";
+import type {
+  MapViewProps,
+  MapCalloutProps,
+  MapMarkerProps,
+} from "react-native-maps";
 
-let MapView: React.ElementType | undefined,
-  Callout: React.ElementType | undefined,
-  Marker: React.ElementType | undefined;
+let MapView: React.ElementType<MapViewProps>;
+let Callout: React.ElementType<MapCalloutProps>;
+let Marker: React.ElementType<MapMarkerProps>;
 
 // Importing react-native-maps when using @draftbit/ui on Web Snack causes errors
 // .web and .native files are not sufficient for this problem
@@ -10,6 +15,10 @@ if (Platform.OS !== "web") {
   MapView = require("react-native-maps").default;
   Callout = require("react-native-maps").Callout;
   Marker = require("react-native-maps").Marker;
+} else {
+  MapView = require("@teovilla/react-native-web-maps").default;
+  Callout = require("@teovilla/react-native-web-maps").Callout;
+  Marker = require("@teovilla/react-native-web-maps").Marker;
 }
 
 export default MapView;

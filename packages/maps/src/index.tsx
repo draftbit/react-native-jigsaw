dc656230b03db52f645b2fb95a0dc9bdb1fd8fb5
Youssef Henna <youssef.hisham14@gmail.com>
2023-05-11 12:06:50 +0300

    Export callout directly

--- packages/maps/src/index.tsx
+++ packages/maps/src/index.tsx
@@ -1,1 +1,4 @@
-export { MapView, MapMarker, MapCallout } from "./components";
+export { MapView, MapMarker } from "./components";
+
+// Has to be exported directly, otherwise can be considered a custom marker and not a callout
+export { Callout as MapCallout } from "./components/react-native-maps";

4149b48011609c3808594f1eca88cb928a2d72c5
Youssef Henna <youssef.hisham14@gmail.com>
2023-05-15 11:09:26 +0300

    Explcitly set function name to 'Callout'

--- packages/maps/src/components/MapCallout.tsx
+++ packages/maps/src/components/MapCallout.tsx
@@ -9,9 +9,13 @@ export interface MapCalloutProps
 
 // Has to be a function named 'Callout' to be matched as a Callout component and not a custom view for marker
 // See: https://github.com/teovillanueva/react-native-web-maps/blob/81278079c6f26a707d915d69de9a00080c305957/packages/react-native-web-maps/src/components/marker.web.tsx#L79
-export function Callout({
+function Callout({
   showTooltip,
   ...rest
 }: React.PropsWithChildren<MapCalloutProps>) {
   return <MapCalloutComponent tooltip={!showTooltip} {...rest} />;
 }
+
+Callout.name = "Callout";
+
+export { Callout };

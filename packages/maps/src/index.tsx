8324e3e7d36b014e608b4fef258706a3e9d34f74
Youssef Henna <youssef.hisham14@gmail.com>
2023-05-15 19:35:58 +0300

    Updated how markers & callouts are rendered to maintain types + added default callout for title/description

--- packages/maps/src/index.tsx
+++ packages/maps/src/index.tsx
@@ -1,3 +1,3 @@
 export { default as MapView } from "./components/MapView";
 export { default as MapMarker } from "./components/MapMarker";
-export { Callout as MapCallout } from "./components/MapCallout";
+export { default as MapCallout } from "./components/MapCallout";

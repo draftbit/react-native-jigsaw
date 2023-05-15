429b0189855aeb4132c231aa237bcadf9b551f5b
Youssef Henna <youssef.hisham14@gmail.com>
2023-05-15 19:35:58 +0300

    Updated how markers & callouts are rendered to maintain types + added default callout for title/description

--- packages/maps/src/components/MapCallout.tsx
+++ packages/maps/src/components/MapCallout.tsx
@@ -1,17 +1,27 @@
 import * as React from "react";
-import { Callout as MapCalloutComponent } from "./react-native-maps";
 import type { MapCalloutProps as MapCalloutComponentProps } from "react-native-maps";
+import { Callout as MapCalloutComponent } from "./react-native-maps";
 
 export interface MapCalloutProps
   extends Omit<MapCalloutComponentProps, "tooltip"> {
   showTooltip?: boolean;
 }
 
-// Has to be a function named 'Callout' to be matched as a Callout component and not a custom view for marker
-// See: https://github.com/teovillanueva/react-native-web-maps/blob/81278079c6f26a707d915d69de9a00080c305957/packages/react-native-web-maps/src/components/marker.web.tsx#L79
-export function Callout({
-  showTooltip,
-  ...rest
-}: React.PropsWithChildren<MapCalloutProps>) {
-  return <MapCalloutComponent tooltip={!showTooltip} {...rest} />;
+/**
+ * Renders nothing, serves as placeholder for props
+ * Rendering exposed as function to avoid having an intermediary component that changes the type
+ *
+ * This is done because the underlying package has logic dependant on the type of child
+ * See: https://github.com/teovillanueva/react-native-web-maps/blob/5f3d0ec7c24f789c3df30c1d6d7223e638ff5868/packages/react-native-web-maps/src/components/marker.web.tsx#L79
+ */
+const MapCallout: React.FC<React.PropsWithChildren<MapCalloutProps>> = () => {
+  return null;
+};
+
+export function renderCallout(props: MapCalloutProps, key: React.Key) {
+  return (
+    <MapCalloutComponent key={key} tooltip={!props.showTooltip} {...props} />
+  );
 }
+
+export default MapCallout;

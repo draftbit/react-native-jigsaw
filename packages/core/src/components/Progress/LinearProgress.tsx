70981947fb0bc2a0105cb5f2f15e6b81297c1708
Youssef Henna <youssef.hisham14@gmail.com>
2023-05-21 13:27:07 +0300

    Finalized Linear Progress

--- packages/core/src/components/Progress/LinearProgress.tsx
+++ packages/core/src/components/Progress/LinearProgress.tsx
@@ -9,7 +9,6 @@ import Animated, {
   withTiming,
 } from "react-native-reanimated";
 
-//TODO: Allow stroke gaps and widths
 type LineCap = "round" | "square";
 
 interface LinearProgressProps {
@@ -27,6 +26,14 @@ interface LinearProgressProps {
   isAnimated?: boolean;
   lineCap?: LineCap;
   trackLineCap?: LineCap;
+  dashWidth?: string | number;
+  trackDashWidth?: string | number;
+  dashGap?: string | number;
+  trackDashGap?: string | number;
+  dashOffset?: string | number;
+  trackDashOffset?: string | number;
+  customDashArray?: string;
+  trackCustomDashArray?: string;
   style?: StyleProp<ViewStyle>;
   theme: Theme;
 }
@@ -38,47 +45,61 @@ const LinearProgress: React.FC<LinearProgressProps> = ({
   value,
   minimumValue = 0,
   maximumValue = 100,
-  initialValueToAnimateFrom: initialValue = minimumValue,
+  initialValueToAnimateFrom = minimumValue,
   thickness = 10,
   trackThickness = thickness,
   color = theme.colors.primary,
   trackColor = theme.colors.divider,
   trackOpacity = 1,
   showTrack = true,
-  animationDuration = 2000,
-  // isAnimated = true,
+  animationDuration = 500,
+  isAnimated = true,
   lineCap = "round",
-  trackLineCap = "round",
-
+  trackLineCap = lineCap,
+  dashWidth,
+  trackDashWidth,
+  dashGap,
+  trackDashGap,
+  dashOffset,
+  trackDashOffset,
+  customDashArray,
+  trackCustomDashArray,
   style,
 }) => {
   const [svgContainerWidth, setSvgContainerWidth] = React.useState(0);
 
-  //This offset guarantees nothing is cut off by view bounds
-  const thicknessOffset = thickness / 2;
-  const trackThicknessOffset = trackThickness / 2;
-  const maxThickness = Math.max(thicknessOffset, trackThicknessOffset);
+  const dashArray =
+    dashWidth !== undefined
+      ? `${dashWidth} ${dashGap || dashWidth}`
+      : undefined;
+  const trackDashArray =
+    trackDashWidth !== undefined
+      ? `${trackDashWidth} ${trackDashGap || trackDashWidth}`
+      : undefined;
+
+  const maxThickness = Math.max(thickness, trackThickness);
+  const thicknessOffset = maxThickness / 2; // This offset guarantees nothing is cut off by view bounds
 
   const progressLineWidth = svgContainerWidth - thicknessOffset;
-  const trackProgressLineWidth = svgContainerWidth - trackThicknessOffset;
+  const trackProgressLineWidth = svgContainerWidth - thicknessOffset;
 
-  const currentProgressLineWidth = useSharedValue(initialValue);
+  const currentProgressLineWidth = useSharedValue(initialValueToAnimateFrom);
 
   const progressLineAnimatedProps = useAnimatedProps<LineProps>(() => {
+    const isBelowMinWidth = currentProgressLineWidth.value <= thicknessOffset;
     return {
-      x2: currentProgressLineWidth.value,
+      x2: Math.min(progressLineWidth, currentProgressLineWidth.value), //Prevents going beyond the max width
+      strokeOpacity: isBelowMinWidth ? 0.0 : 1.0,
     };
   });
 
   React.useEffect(() => {
-    const newLineWidth =
-      progressLineWidth * (value / (maximumValue + minimumValue)) +
-      thicknessOffset;
-    console.log(newLineWidth);
-
-    currentProgressLineWidth.value = withTiming(newLineWidth, {
-      duration: animationDuration,
-    });
+    currentProgressLineWidth.value = withTiming(
+      progressLineWidth * (value / (maximumValue + minimumValue)),
+      {
+        duration: isAnimated ? animationDuration : 0,
+      }
+    );
   }, [
     value,
     progressLineWidth,
@@ -86,7 +107,7 @@ const LinearProgress: React.FC<LinearProgressProps> = ({
     currentProgressLineWidth,
     maximumValue,
     minimumValue,
-    thicknessOffset,
+    isAnimated,
   ]);
 
   return (
@@ -94,31 +115,35 @@ const LinearProgress: React.FC<LinearProgressProps> = ({
       onLayout={(event) => setSvgContainerWidth(event.nativeEvent.layout.width)}
       style={[
         {
-          height: Math.max(thickness, trackThickness),
+          height: maxThickness,
         },
         style,
       ]}
     >
       {showTrack && (
         <Line
-          x1={trackThicknessOffset}
-          y1={maxThickness}
+          x1={thicknessOffset}
+          y1={thicknessOffset}
           x2={trackProgressLineWidth}
-          y2={maxThickness}
+          y2={thicknessOffset}
           stroke={trackColor}
           strokeWidth={trackThickness}
           strokeOpacity={trackOpacity}
           strokeLinecap={trackLineCap}
+          strokeDasharray={trackCustomDashArray || trackDashArray}
+          strokeDashoffset={trackDashOffset}
         />
       )}
       <AnimatedLine
         animatedProps={progressLineAnimatedProps}
         x1={thicknessOffset}
-        y1={maxThickness}
-        y2={maxThickness}
+        y1={thicknessOffset}
+        y2={thicknessOffset}
         stroke={color}
         strokeWidth={thickness}
         strokeLinecap={lineCap}
+        strokeDasharray={customDashArray || dashArray}
+        strokeDashoffset={dashOffset}
       />
     </Svg>
   );

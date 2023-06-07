bb17d517efd63b52983d86dd31ab7c891b4ae340
Youssef Henna <youssef.hisham14@gmail.com>
2023-06-07 13:13:40 +0300

    Address renimated svg issue: https://github.com/software-mansion/react-native-reanimated/issues/3321#issuecomment-1256983430

--- packages/core/src/components/Progress/LinearProgress/LinearProgress.tsx
+++ packages/core/src/components/Progress/LinearProgress/LinearProgress.tsx
@@ -127,6 +127,7 @@ export const LinearProgress: React.FC<ValueProgressProps> = ({
           strokeLinecap={lineCap}
           strokeDasharray={customDashArray || dashArray}
           strokeDashoffset={dashOffset}
+          onPress={() => {}} //Addresses reanimated issue with SVG (https://github.com/software-mansion/react-native-reanimated/issues/3321#issuecomment-1256983430)
         />
       </Svg>
     </View>

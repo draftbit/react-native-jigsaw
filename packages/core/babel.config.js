c8cf8f40ed90c0789a9bb438b26c3befa5d756d7
Youssef Henna <youssef.hisham14@gmail.com>
2023-06-08 20:00:10 +0300

    Use babel to compile packages

--- packages/core/babel.config.js
+++ packages/core/babel.config.js
@@ -1,4 +1,9 @@
 module.exports = {
   presets: ["module:metro-react-native-babel-preset"],
-  plugins: ["react-native-reanimated/plugin"],
+  plugins: [
+    "@babel/plugin-proposal-export-namespace-from",
+    "@babel/plugin-proposal-class-properties",
+    "react-native-reanimated/plugin",
+  ],
+  ignore: ["**/__tests__"],
 };

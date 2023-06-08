711a1472dc740e74a073f87bf4f809476a493636
Youssef Henna <youssef.hisham14@gmail.com>
2023-06-08 20:21:36 +0300

    Remove ignore tests in babel config

--- packages/core/babel.config.js
+++ packages/core/babel.config.js
@@ -2,8 +2,6 @@ module.exports = {
   presets: ["module:metro-react-native-babel-preset"],
   plugins: [
     "@babel/plugin-proposal-export-namespace-from",
-    "@babel/plugin-proposal-class-properties",
     "react-native-reanimated/plugin",
   ],
-  ignore: ["**/__tests__"],
 };

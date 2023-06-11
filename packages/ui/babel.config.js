55ee080d5e0707dfbd3fe654a8b44039eca80048
Youssef Henna <youssef.hisham14@gmail.com>
2023-06-11 11:40:31 +0300

    Revert "Updated babel configs"
    This reverts commit 42b84d90d3ad07cb7ce8b746f8f6b4d2b66fe501.

--- packages/ui/babel.config.js
+++ packages/ui/babel.config.js
@@ -1,10 +1,5 @@
 module.exports = {
-  presets: [
-    "@babel/preset-env",
-    "@babel/preset-react",
-    "@babel/preset-typescript",
-    "module:metro-react-native-babel-preset",
-  ],
+  presets: ["module:metro-react-native-babel-preset"],
   plugins: [
     "@babel/plugin-proposal-export-namespace-from",
     "react-native-reanimated/plugin",

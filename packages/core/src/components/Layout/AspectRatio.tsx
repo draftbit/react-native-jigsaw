7016de6609a0f5e8b95c75709aaaa68f53e0f68d
Youssef Henna <youssef.hisham14@gmail.com>
2023-07-17 20:10:11 +0300

    Fixed aspect ratio component

--- packages/core/src/components/Layout/AspectRatio.tsx
+++ packages/core/src/components/Layout/AspectRatio.tsx
@@ -5,37 +5,12 @@ interface AspectRatioProps extends ViewProps {
   aspectRatio?: number;
 }
 
-//TODO: Fix aspect ration when only height is provided
 const AspectRatio: React.FC<AspectRatioProps> = ({
   aspectRatio = 1,
   style,
-  onLayout,
   ...rest
 }) => {
-  const [calculatedWidth, setCalculatedWidth] = React.useState(0);
-  const [calculatedHeight, setCalculatedHeight] = React.useState(0);
-
-  let width = calculatedWidth;
-  let height = calculatedHeight;
-
-  if (calculatedWidth) {
-    height = width / aspectRatio;
-  } else {
-    width = height * aspectRatio;
-  }
-
-  return (
-    <View
-      onLayout={(e) => {
-        const layout = e.nativeEvent.layout;
-        setCalculatedWidth(layout.width);
-        setCalculatedHeight(layout.height);
-        onLayout?.(e);
-      }}
-      {...rest}
-      style={[{ width, height }, style]}
-    />
-  );
+  return <View {...rest} style={[{ aspectRatio }, style]} />;
 };
 
 export default AspectRatio;

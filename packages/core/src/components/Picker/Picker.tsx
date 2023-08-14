1c5cd59d445ffe3bf5d2d57733a0b7e5a5266433
Youssef Henna <youssef.hisham14@gmail.com>
2023-08-14 11:59:22 +0300

    Moved Picker text field/input into a seperate component

--- packages/core/src/components/Picker/Picker.tsx
+++ packages/core/src/components/Picker/Picker.tsx
@@ -1,53 +1,13 @@
 import * as React from "react";
-import {
-  View,
-  StyleSheet,
-  Platform,
-  ViewStyle,
-  StyleProp,
-  Keyboard,
-} from "react-native";
+import { View, StyleSheet, Platform, Keyboard } from "react-native";
 import { isObject } from "lodash";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { Picker as NativePicker } from "@react-native-picker/picker";
 import Portal from "../Portal/Portal";
 import { Button } from "../Button";
-import Touchable from "../Touchable";
-import type { IconSlot } from "../../interfaces/Icon";
-import {
-  extractBorderAndMarginStyles,
-  extractSizeStyles,
-  extractFlexItemStyles,
-  extractPositionStyles,
-  useDeepCompareMemo,
-} from "../../utilities";
-import TextField from "../TextField";
-import omit from "lodash.omit";
-
-export interface PickerOption {
-  value: string;
-  label: string;
-}
-
-export type PickerProps = {
-  error?: any;
-  placeholder?: string;
-  disabled?: boolean;
-  style?: StyleProp<ViewStyle> & { height?: number };
-  value?: string;
-  options: PickerOption[] | string[];
-  onValueChange: (value: string) => void;
-  assistiveText?: string;
-  label?: string;
-  iconColor?: string;
-  iconSize?: number;
-  leftIconMode?: "inset" | "outset";
-  leftIconName?: string;
-  placeholderTextColor?: string;
-  rightIconName?: string;
-  type?: "solid" | "underline";
-  autoDismissKeyboard?: boolean;
-} & IconSlot;
+import { useDeepCompareMemo } from "../../utilities";
+import { PickerOption, PickerProps } from "./PickerCommon";
+import PickerInputContainer from "./PickerInputContainer";
 
 const isIos = Platform.OS === "ios";
 const isWeb = Platform.OS === "web";
@@ -56,14 +16,12 @@ const Picker: React.FC<PickerProps> = ({
   options: optionsProp = [],
   onValueChange,
   Icon,
-  style,
   placeholder,
   value,
-  disabled = false,
   autoDismissKeyboard = true,
   ...rest
 }) => {
-  const pickerRef = React.useRef<NativePicker<string>>(null);
+  const pickerRef = React.useRef<NativePicker<string | number>>(null);
 
   const [pickerVisible, setPickerVisible] = React.useState(false);
 
@@ -84,19 +42,6 @@ const Picker: React.FC<PickerProps> = ({
     onValueChange?.(options[0].value);
   }
 
-  const selectedLabel =
-    options.find((option) => option.value === String(value))?.label ||
-    value ||
-    placeholder;
-
-  const containerStyle = StyleSheet.flatten([
-    extractSizeStyles(style),
-    extractPositionStyles(style),
-    extractFlexItemStyles(style),
-    extractBorderAndMarginStyles(style).marginStyles,
-  ]);
-  const textFieldStyle = omit(style, Object.keys(containerStyle));
-
   const renderNativePicker = () => (
     <NativePicker
       ref={pickerRef}
@@ -116,7 +61,7 @@ const Picker: React.FC<PickerProps> = ({
     >
       {options.map((option) => (
         <NativePicker.Item
-          label={option.label}
+          label={option.label.toString()}
           value={option.value}
           key={option.value}
         />
@@ -161,26 +106,17 @@ const Picker: React.FC<PickerProps> = ({
   }, [pickerVisible, autoDismissKeyboard]);
 
   return (
-    <View style={containerStyle}>
-      <Touchable
-        disabled={disabled}
-        onPress={() => setPickerVisible(!pickerVisible)}
-      >
-        <TextField
-          Icon={Icon}
-          numberOfLines={1}
-          onChangeText={() => {}}
-          value={String(selectedLabel ?? placeholder)}
-          editable={false}
-          disabled={disabled}
-          style={textFieldStyle}
-          {...rest}
-        />
-      </Touchable>
-
+    <PickerInputContainer
+      Icon={Icon}
+      placeholder={placeholder}
+      selectedValue={value}
+      options={options}
+      onPress={() => setPickerVisible(!pickerVisible)}
+      {...rest}
+    >
       {/* Web version is collapsed by default, always show to allow direct expand */}
       {(pickerVisible || isWeb) && renderPicker()}
-    </View>
+    </PickerInputContainer>
   );
 };
 
@@ -221,22 +157,22 @@ function normalizeToPickerOptions(
 
   if (typeof firstOption === ("string" || "number")) {
     return options.map((option) => ({
-      label: String(option),
-      value: String(option),
+      label: option as string | number,
+      value: option as string | number,
     }));
   }
 
   if (isObject(firstOption) && firstOption.value && firstOption.label) {
     return (options as PickerOption[]).map((option) => {
       return {
-        label: String(option.label),
-        value: String(option.value),
+        label: option.label,
+        value: option.value,
       };
     });
   }
 
   throw new Error(
-    'Picker options must be either an array of strings or array of { "label": string; "value": string; } objects.'
+    'Picker options must be either an array of strings, numbers, or an array of { "label": string | number; "value": string | number; } objects.'
   );
 }
 

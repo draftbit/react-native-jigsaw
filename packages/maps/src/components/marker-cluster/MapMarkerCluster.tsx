6eb936b9afe152d8e3ccf6ecace6115367b343a8
Youssef Henna <youssef.hisham14@gmail.com>
2023-10-05 12:19:41 +0300

    Revert "Disable `tracksViewChanges` on marker clusters (#797)"
    This reverts commit 003c56cfac311deb201058dd1858188925ac68d0.

--- packages/maps/src/components/marker-cluster/MapMarkerCluster.tsx
+++ packages/maps/src/components/marker-cluster/MapMarkerCluster.tsx
@@ -74,7 +74,6 @@ const MapMarkerCluster: React.FC<React.PropsWithChildren> = ({
                   latitude,
                   longitude,
                   children: clusterView,
-                  tracksViewChanges: false,
                   onPress,
                 })}
               </MapMarkerClusterContext.Provider>

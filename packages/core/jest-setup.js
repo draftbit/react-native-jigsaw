7fc0d8c66429d94464dab54ccd0b4b4287866e71
Youssef Henna <youssef.hisham14@gmail.com>
2023-10-29 16:24:52 +0200

    Upgrade jest-expo + fix tests

--- packages/core/jest-setup.js
+++ packages/core/jest-setup.js
@@ -1,5 +1,5 @@
 import "@shopify/flash-list/jestSetup";
-import { setUpTests as setupReaanimatedTests } from "react-native-reanimated/lib/reanimated2/jestUtils";
+import { setUpTests as setupReaanimatedTests } from "react-native-reanimated/src/reanimated2/jestUtils";
 setupReaanimatedTests();
 
 // Fixes reanimated jest bug: https://github.com/software-mansion/react-native-reanimated/issues/3125

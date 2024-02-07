6c8e2d6e173dcbc5b5ec79f70b8b64c84b7e8957
Youssef Henna <youssef.hisham14@gmail.com>
2024-02-07 20:16:55 +0200

    Fix to tests

--- packages/ui/babel.config.js
+++ packages/ui/babel.config.js
@@ -1,18 +1,23 @@
-module.exports = {
-  presets: [
-    [
-      "module:metro-react-native-babel-preset",
-      {
-        /**
-         * Addresses issue caused on snack that leads to `Unable to resolve module 'app/node_modules/@babel/runtime/helpers/interopRequireDefault.js'`
-         * See: https://github.com/expo/snack/pull/302
-         */
-        disableImportExportTransform: true,
-      },
+module.exports = (api) => {
+  const isTest = api.env("test");
+
+  return {
+    presets: [
+      [
+        "module:metro-react-native-babel-preset",
+        {
+          /**
+           * Addresses issue caused on snack that leads to `Unable to resolve module 'app/node_modules/@babel/runtime/helpers/interopRequireDefault.js'`
+           * See: https://github.com/expo/snack/pull/302
+           * Always false for tests for jest to work
+           */
+          disableImportExportTransform: !isTest,
+        },
+      ],
     ],
-  ],
-  plugins: [
-    "@babel/plugin-proposal-export-namespace-from",
-    "react-native-reanimated/plugin",
-  ],
+    plugins: [
+      "@babel/plugin-proposal-export-namespace-from",
+      "react-native-reanimated/plugin",
+    ],
+  };
 };

31a19023f47748ebb73e23239abbda257ab93489
Youssef Henna <youssef.hisham14@gmail.com>
2024-02-07 18:23:18 +0200

    `disableImportExportTransform` in babel config

--- packages/native/babel.config.js
+++ packages/native/babel.config.js
@@ -1,3 +1,14 @@
 module.exports = {
-  presets: ["module:metro-react-native-babel-preset"],
+  presets: [
+    [
+      "module:metro-react-native-babel-preset",
+      {
+        /**
+         * Addresses issue caused on snack that leads to `Unable to resolve module 'app/node_modules/@babel/runtime/helpers/interopRequireDefault.js'`
+         * See: https://github.com/expo/snack/pull/302
+         */
+        disableImportExportTransform: true,
+      },
+    ],
+  ],
 };
--- packages/ui/babel.config.js
+++ packages/ui/babel.config.js
@@ -1,5 +1,16 @@
 module.exports = {
-  presets: ["module:metro-react-native-babel-preset"],
+  presets: [
+    [
+      "module:metro-react-native-babel-preset",
+      {
+        /**
+         * Addresses issue caused on snack that leads to `Unable to resolve module 'app/node_modules/@babel/runtime/helpers/interopRequireDefault.js'`
+         * See: https://github.com/expo/snack/pull/302
+         */
+        disableImportExportTransform: true,
+      },
+    ],
+  ],
   plugins: [
     "@babel/plugin-proposal-export-namespace-from",
     "react-native-reanimated/plugin",

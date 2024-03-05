d34433e6f00f404bd27b087c9b1dbe28806f36bc
Youssef Henna <youssef.hisham14@gmail.com>
2024-03-05 12:48:42 +0200

    Use hook with measured size to calculate styles

--- packages/core/src/components/SimpleStyleScrollables/SimpleStyleSwipeableList.tsx
+++ packages/core/src/components/SimpleStyleScrollables/SimpleStyleSwipeableList.tsx
@@ -4,7 +4,7 @@ import type {
   FlashListSwipeableListProps,
   FlatListSwipeableListProps,
 } from "../SwipeableItem";
-import splitContentContainerStyles from "./splitContentContainerStyles";
+import useSplitContentContainerStyles from "./useSplitContentContainerStyles";
 
 /**
  * A SwipeableList wrapper that takes a single `style` prop and internally extracts
@@ -17,12 +17,22 @@ const SimpleStyleSwipeableList = <T extends { [key: string]: any }>({
   FlashListSwipeableListProps<T> | FlatListSwipeableListProps<T>,
   "contentContainerStyle"
 >) => {
-  const { style, contentContainerStyle } =
-    splitContentContainerStyles(styleProp);
+  const [measuredWidth, setMeasuredWidth] = React.useState<number>();
+  const [measuredHeight, setMeasuredHeight] = React.useState<number>();
+
+  const { style, contentContainerStyle } = useSplitContentContainerStyles(
+    styleProp,
+    measuredWidth,
+    measuredHeight
+  );
 
   return (
     //@ts-ignore contentContainerStyle has different types for FlashList and FlatList implmentations and confuses TS
     <SwipeableList
+      onLayout={(event) => {
+        setMeasuredWidth(event.nativeEvent.layout.width);
+        setMeasuredHeight(event.nativeEvent.layout.height);
+      }}
       style={style}
       contentContainerStyle={contentContainerStyle}
       {...rest}

77950fcc62bffb41f8d2d2c6fe246f44d9458ca9
Youssef Henna <youssef.hisham14@gmail.com>
2024-03-06 18:51:44 +0200

    Add `webShowOutline` prop to text input

--- packages/core/src/components/TextInput.tsx
+++ packages/core/src/components/TextInput.tsx
@@ -2,16 +2,28 @@ import React from "react";
 import {
   TextInput as NativeTextInput,
   TextInputProps as NativeTextInputProps,
+  Platform,
 } from "react-native";
 import { useDebounce, useOnUpdate } from "../hooks";
 
 export interface TextInputProps extends NativeTextInputProps {
+  webShowOutline?: boolean;
   onChangeTextDelayed?: (text: string) => void;
   changeTextDelay?: number;
 }
 
 const TextInput = React.forwardRef<NativeTextInput, TextInputProps>(
-  ({ onChangeTextDelayed, changeTextDelay = 500, value, ...rest }, ref) => {
+  (
+    {
+      onChangeTextDelayed,
+      changeTextDelay = 500,
+      webShowOutline = true,
+      style,
+      value,
+      ...rest
+    },
+    ref
+  ) => {
     const delayedValue = useDebounce(value, changeTextDelay);
 
     useOnUpdate(() => {
@@ -25,6 +37,11 @@ const TextInput = React.forwardRef<NativeTextInput, TextInputProps>(
         testID="native-text-input"
         ref={ref}
         value={value}
+        style={[
+          //@ts-ignore Web specific prop. Removes default blue outline that appears on the hidden TextInput
+          Platform.OS === "web" && !webShowOutline ? { outlineWidth: 0 } : {},
+          style,
+        ]}
         {...rest}
       />
     );

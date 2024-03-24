c0638b3362d8639859152a9c46ceced863840f9c
Youssef Henna <youssef.hisham14@gmail.com>
2024-03-24 15:46:40 +0200

    Allow multiple text nodes in markdown component

--- packages/core/src/components/Markdown.tsx
+++ packages/core/src/components/Markdown.tsx
@@ -6,15 +6,34 @@ type MarkdownProps = {
   style?: StyleProp<ViewStyle>;
 };
 
+const childToString = (child?: React.ReactNode): string => {
+  if (
+    typeof child === "undefined" ||
+    child === null ||
+    typeof child === "boolean"
+  ) {
+    return "";
+  }
+
+  if (JSON.stringify(child) === "{}") {
+    return "";
+  }
+
+  return (child as number | string).toString();
+};
+
 const Markdown: React.FC<React.PropsWithChildren<MarkdownProps>> = ({
-  children,
+  children: childrenProp,
   style,
 }) => {
+  const children = React.Children.toArray(childrenProp);
+  const text = children.map(childToString).join("");
+
   return (
     //'body' style applies to all markdown components
     //@ts-ignore TS does not like the type of this named style for some reason
     <MarkdownComponent style={{ body: StyleSheet.flatten(style) }}>
-      {children}
+      {text}
     </MarkdownComponent>
   );
 };

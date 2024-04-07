1646d4210974ca424984567db84fbdb12e65e5ad
Youssef Henna <youssef.hisham14@gmail.com>
2024-04-07 13:32:46 +0200

    Revert "Remove `disableImportExportTransform` from babel"
    This reverts commit 71e26d0763102ebfa41ca557c32c3509232b7ecb.

--- packages/native/babel.config.js
+++ packages/native/babel.config.js
@@ -2,6 +2,18 @@ module.exports = (api) => {
   const isTest = api.env("test");
 
   return {
-    presets: ["module:metro-react-native-babel-preset"],
+    presets: [
+      [
+        "module:metro-react-native-babel-preset",
+        {
+          /**
+           * Addresses issue caused on snack that leads to `Unable to resolve module 'app/node_modules/@babel/runtime/helpers/interopRequireDefault.js'`
+           * See: https://github.com/expo/snack/pull/302
+           * Always false for tests for jest to work
+           */
+          disableImportExportTransform: !isTest,
+        },
+      ],
+    ],
   };
 };
--- packages/ui/babel.config.js
+++ packages/ui/babel.config.js
@@ -2,7 +2,19 @@ module.exports = (api) => {
   const isTest = api.env("test");
 
   return {
-    presets: ["module:metro-react-native-babel-preset"],
+    presets: [
+      [
+        "module:metro-react-native-babel-preset",
+        {
+          /**
+           * Addresses issue caused on snack that leads to `Unable to resolve module 'app/node_modules/@babel/runtime/helpers/interopRequireDefault.js'`
+           * See: https://github.com/expo/snack/pull/302
+           * Always false for tests for jest to work
+           */
+          disableImportExportTransform: !isTest,
+        },
+      ],
+    ],
     plugins: [
       "@babel/plugin-proposal-export-namespace-from",
       "react-native-reanimated/plugin",

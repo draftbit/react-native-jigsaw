71e26d0763102ebfa41ca557c32c3509232b7ecb
Youssef Henna <youssef.hisham14@gmail.com>
2024-04-07 13:21:15 +0200

    Remove `disableImportExportTransform` from babel

--- packages/types/babel.config.js
+++ packages/types/babel.config.js
@@ -2,18 +2,6 @@ module.exports = (api) => {
   const isTest = api.env("test");
 
   return {
-    presets: [
-      [
-        "module:metro-react-native-babel-preset",
-        {
-          /**
-           * Addresses issue caused on snack that leads to `Unable to resolve module 'app/node_modules/@babel/runtime/helpers/interopRequireDefault.js'`
-           * See: https://github.com/expo/snack/pull/302
-           * Always false for tests for jest to work
-           */
-          disableImportExportTransform: !isTest,
-        },
-      ],
-    ],
+    presets: ["module:metro-react-native-babel-preset"],
   };
 };

d5a97de12f9a295da33e6d9441b7fce3c99d7c55
Youssef Henna <youssef.hisham14@gmail.com>
2024-04-18 14:04:08 +0200

    Initial progress on new themeing system

--- packages/theme/src/styles/DefaultTheme.tsx
+++ packages/theme/src/styles/DefaultTheme.tsx
@@ -1,6 +1,119 @@
 import { systemWeights } from "react-native-typography";
 import configureFonts from "./fonts";
 
+// Each of these colors should have a counterpart in new themes for migration be possible
+
+type ThemeValues = {
+  [key: string]: string | number | ThemeValues;
+};
+
+type NewTheme = {
+  name: string;
+  colors: {
+    branding?: ThemeValues;
+    text?: ThemeValues;
+    background?: ThemeValues;
+    foreground?: ThemeValues;
+    border?: ThemeValues;
+  };
+  typography: { [key: string]: any }; //TODO: Update theme to support typography. Left as 'any' for legacy support until updated and migrated
+};
+
+type ColorPaletteValues = {
+  [key: string]: string;
+};
+
+type ColorPalettes = {
+  [key: string]: ColorPaletteValues;
+};
+
+type Breakpoints = {
+  [key: string]: number;
+};
+
+function validatePalettes(palettes: ColorPalettes) {
+  for (const key of Object.keys(palettes)) {
+    if (typeof key !== "string") {
+      throw new Error(`Invalid palettes object, ${key} is not a string`);
+    }
+    for (const [innerKey, value] of Object.entries(palettes[key])) {
+      if (typeof innerKey !== "string") {
+        throw new Error(
+          `Invalid palette color key, ${innerKey} is not a string`
+        );
+      }
+      if (typeof value !== "string") {
+        throw new Error(
+          `Invalid palette color value, ${value} is not a string`
+        );
+      }
+    }
+  }
+
+  return palettes;
+}
+
+function validateBreakpoints(breakpoints: Breakpoints) {
+  for (const key of Object.keys(breakpoints)) {
+    if (typeof key !== "string") {
+      throw new Error(`Invalid breakpoints object, ${key} is not a string`);
+    }
+    for (const [innerKey, value] of Object.entries(breakpoints[key])) {
+      if (typeof innerKey !== "string") {
+        throw new Error(`Invalid breakpoint key, ${innerKey} is not a string`);
+      }
+      if (typeof value !== "number") {
+        throw new Error(`Invalid breakpoint value, ${value} is not a number`);
+      }
+    }
+  }
+}
+
+function validateTheme(theme: NewTheme, breakpoints: Breakpoints) {
+  const reservedPlatformKeys = ["ios", "android", "web", "macos", "windows"];
+  const breakpointKeys = Object.keys(breakpoints);
+
+  // TODO: validate correct format for theme (not colors only)
+  // Make sure reserved keys are not used, otherwise throw an error
+}
+
+//TODO: Context and providers (can access current theme) and utility to switch themes at any point
+// Use name as theme identifier to change theme. Validate no 2 themes with same name
+
+function createTheme({
+  breakpoints,
+  palettes,
+  theme,
+  baseTheme,
+}: {
+  breakpoints: Breakpoints;
+  palettes: ColorPalettes;
+  theme: NewTheme;
+  baseTheme?: NewTheme;
+}): NewTheme {
+  validateBreakpoints(breakpoints);
+  validatePalettes(palettes);
+  validateTheme(theme, breakpoints);
+
+  //TODO: Use base theme and duplicate
+
+  return theme;
+}
+
+const theme: NewTheme = {
+  name: "My theme",
+  colors: {
+    branding: {
+      hello: "",
+    },
+    text: {},
+    background: {},
+    foreground: {},
+    border: {},
+  },
+  typography: {},
+};
+
 const DefaultTheme = {
   disabledOpacity: 0.5,
   roundness: 8,

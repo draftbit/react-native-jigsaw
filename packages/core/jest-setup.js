726f95c9f038e68f07289f78c23ed98ec4286aa1
Youssef Henna <youssef.hisham14@gmail.com>
2024-04-21 14:41:51 +0200

    Add mocks to fix tests

--- packages/core/jest-setup.js
+++ packages/core/jest-setup.js
@@ -6,3 +6,18 @@ setupReaanimatedTests();
 global.ReanimatedDataMock = {
   now: () => 0,
 };
+
+jest.mock("expo-constants", () => {
+  const actual = jest.requireActual("expo-constants");
+  return { ...actual, experienceUrl: null };
+});
+
+jest.mock("expo-asset", () => {
+  const actual = jest.requireActual("expo-asset");
+  class Asset extends actual.Asset {
+    static fromModule() {
+      return new actual.Asset({ name: "Name", type: "type", uri: "uri" });
+    }
+  }
+  return { ...actual, Asset, getManifestBaseUrl: () => "" };
+});

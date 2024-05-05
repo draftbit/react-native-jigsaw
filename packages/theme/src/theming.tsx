17d59f404934336be24c90584fb0e4eb43905dc3
Youssef Henna <youssef.hisham14@gmail.com>
2024-05-05 14:36:55 +0300

    Cleanup theming proxy

--- packages/theme/src/theming.tsx
+++ packages/theme/src/theming.tsx
@@ -8,6 +8,7 @@ import {
   validateTheme,
 } from "./validators";
 import { Platform } from "react-native";
+import { isObject } from "lodash";
 
 //TODO: Context and providers (can access current theme) and utility to switch themes at any point
 // Use name as theme identifier to change theme. Validate no 2 themes with same name
@@ -42,23 +43,32 @@ export function createTheme({
       branding: createThemeValuesProxy(
         resultTheme.colors.branding,
         breakpoints,
-        400
+        400,
+        "android"
       ),
-      text: createThemeValuesProxy(resultTheme.colors.text, breakpoints, 400),
+      text: createThemeValuesProxy(
+        resultTheme.colors.text,
+        breakpoints,
+        400,
+        "android"
+      ),
       background: createThemeValuesProxy(
         resultTheme.colors.background,
         breakpoints,
-        400
+        400,
+        "android"
       ),
       foreground: createThemeValuesProxy(
         resultTheme.colors.foreground,
         breakpoints,
-        400
+        400,
+        "android"
       ),
       border: createThemeValuesProxy(
         resultTheme.colors.border,
         breakpoints,
-        400
+        400,
+        "android"
       ),
     },
   };
@@ -66,15 +76,13 @@ export function createTheme({
 //TODO: Device width should be dynamic, maybe proxies should be created at provider level,
 // not on theme creation level
 
-//TODO: Clean this up to more readable code and functions
-// type checks used a bunch here and in validators, split to util funcitons
-
 //TODO: Tests for checking correct values returned
 
-function createThemeValuesProxy(
+export function createThemeValuesProxy(
   value: ThemeValues | undefined,
   breakpoints: Breakpoints,
-  deviceWidth: number
+  deviceWidth: number,
+  devicePlatform: typeof Platform.OS
 ) {
   if (value === undefined || value === null) {
     return undefined;
@@ -85,74 +93,38 @@ function createThemeValuesProxy(
       key: string
     ): string | number | ThemeValues | undefined => {
       const currentValue = target[key];
-      if (
-        typeof currentValue === "string" ||
-        typeof currentValue === "number"
-      ) {
+      if (!isObject(currentValue)) {
         return currentValue;
       } else {
         const platformKeys = ["ios", "android", "web", "macos", "windows"];
         const breakpointKeys = Object.keys(breakpoints);
-        const keysType = getLevelKeysType(
+        const keysType = getKeysType(
           currentValue,
           platformKeys,
           breakpointKeys
         );
 
         if (keysType === "default") {
-          return createThemeValuesProxy(currentValue, breakpoints, deviceWidth);
-        } else if (keysType === "platform") {
-          const currentPlatformValue =
-            currentValue[Platform.OS] ?? currentValue.default;
-          if (
-            currentPlatformValue === undefined ||
-            currentPlatformValue === null
-          ) {
-            return currentPlatformValue;
-          } else if (
-            typeof currentPlatformValue === "string" ||
-            typeof currentPlatformValue === "number"
-          ) {
-            return currentPlatformValue;
-          } else {
-            return createThemeValuesProxy(
-              currentPlatformValue,
-              breakpoints,
-              deviceWidth
-            );
-          }
-        } else if (keysType === "breakpoint") {
-          const keysToBreakpointValue: [string, number][] = Object.keys(
-            currentValue
-          ).map((key) => [key, breakpoints[key]]);
-          const orderedBreakpoints = keysToBreakpointValue.sort(
-            ([_, value]) => value
+          return createThemeValuesProxy(
+            currentValue,
+            breakpoints,
+            deviceWidth,
+            devicePlatform
           );
-          let currentBreakpointKey = "";
-          for (const [breakpointKey, breakpointValue] of orderedBreakpoints) {
-            if (deviceWidth >= breakpointValue) {
-              currentBreakpointKey = breakpointKey;
-            }
-          }
-          const currentBreakpointValue =
-            currentValue[currentBreakpointKey] ?? currentValue.default;
-          if (
-            currentBreakpointValue === undefined ||
-            currentBreakpointValue === null
-          ) {
-            return currentBreakpointValue;
-          } else if (
-            typeof currentBreakpointValue === "string" ||
-            typeof currentBreakpointValue === "number"
-          ) {
-            return currentBreakpointValue;
-          } else {
-            return createThemeValuesProxy(
-              currentBreakpointValue,
-              breakpoints,
-              deviceWidth
-            );
-          }
+        } else if (keysType === "platform") {
+          return getPlatformValue(
+            currentValue,
+            breakpoints,
+            deviceWidth,
+            devicePlatform
+          );
+        } else if (keysType === "breakpoint") {
+          return getBreakpointValue(
+            currentValue,
+            breakpoints,
+            deviceWidth,
+            devicePlatform
+          );
         } else {
           return undefined;
         }
@@ -164,18 +136,16 @@ function createThemeValuesProxy(
   });
 }
 
-function getLevelKeysType(
-  currentValue: ThemeValues,
+function getKeysType(
+  value: ThemeValues,
   platformKeys: string[],
   breakpointKeys: string[]
 ): "platform" | "breakpoint" | "default" {
-  const hasPlatformKeys = platformKeys.some(
-    (key) => currentValue[key] !== undefined
-  );
+  const hasPlatformKeys = platformKeys.some((key) => value[key] !== undefined);
   const hasBreakpointKeys = breakpointKeys.some(
-    (key) => currentValue[key] !== undefined
+    (key) => value[key] !== undefined
   );
-  const hasUserDefinedKeys = Object.keys(currentValue).some(
+  const hasUserDefinedKeys = Object.keys(value).some(
     (key) => !platformKeys.includes(key) && !breakpointKeys.includes(key)
   );
 
@@ -195,5 +165,54 @@ function getLevelKeysType(
 function onlyOneTrue(a: boolean, b: boolean, c: boolean) {
   return [a, b, c].filter((x) => x).length === 1;
 }
+
+function getPlatformValue(
+  value: ThemeValues,
+  breakpoints: Breakpoints,
+  deviceWidth: number,
+  devicePlatform: typeof Platform.OS
+) {
+  const currentPlatformValue = value[devicePlatform] ?? value.default;
+  if (!isObject(currentPlatformValue)) {
+    return currentPlatformValue;
+  } else {
+    return createThemeValuesProxy(
+      currentPlatformValue,
+      breakpoints,
+      deviceWidth,
+      devicePlatform
+    );
+  }
+}
+
+function getBreakpointValue(
+  value: ThemeValues,
+  breakpoints: Breakpoints,
+  deviceWidth: number,
+  devicePlatform: typeof Platform.OS
+) {
+  const keysToBreakpointValue: [string, number][] = Object.keys(value).map(
+    (key) => [key, breakpoints[key]]
+  );
+  const orderedBreakpoints = keysToBreakpointValue.sort(([_, val]) => val);
+  let currentBreakpointKey = "";
+  for (const [breakpointKey, breakpointValue] of orderedBreakpoints) {
+    if (deviceWidth >= breakpointValue) {
+      currentBreakpointKey = breakpointKey;
+    }
+  }
+  const currentBreakpointValue = value[currentBreakpointKey] ?? value.default;
+  if (!isObject(currentBreakpointValue)) {
+    return currentBreakpointValue;
+  } else {
+    return createThemeValuesProxy(
+      currentBreakpointValue,
+      breakpoints,
+      deviceWidth,
+      devicePlatform
+    );
+  }
+}
+
 export const { ThemeProvider, withTheme, useTheme } =
   createTheming<Theme>(DefaultTheme);

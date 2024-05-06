357faf47f016f34ed54c33d6c9e231bb0998f493
Youssef Henna <youssef.hisham14@gmail.com>
2024-05-06 12:28:35 +0300

    Add new theme provider and hooks

--- packages/theme/src/index.ts
+++ packages/theme/src/index.ts
@@ -1,3 +1,9 @@
 export { default as DefaultTheme } from "./DefaultTheme";
-export type { Theme, Breakpoints, ColorPalettes } from "./types";
-export { ThemeProvider, useTheme, withTheme } from "./theming";
+export type { Theme, ReadTheme, Breakpoints, ColorPalettes } from "./types";
+export { default as createTheme } from "./createTheme";
+export {
+  Provider as ThemeProvider,
+  useChangeTheme,
+  useTheme,
+  withTheme,
+} from "./Provider";

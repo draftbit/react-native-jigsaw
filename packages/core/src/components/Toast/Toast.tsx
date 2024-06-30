48cc21608056a8226be7a36194e969f61eda1cf3
sieu-db <sieu@draftbit.com>
2024-07-01 00:40:26 +0700

    chore: add some test props

--- packages/core/src/components/Toast/Toast.tsx
+++ packages/core/src/components/Toast/Toast.tsx
@@ -1,12 +1,26 @@
 import React from "react";
+import { StyleProp, TextStyle, ViewStyle, StyleSheet } from "react-native";
 import { View, Text } from "react-native";
+import { extractBorderAndMarginStyles } from "../../utilities";
 
-type Props = {};
+type Props = {
+  visible: boolean;
+  labelStyle?: StyleProp<TextStyle>;
+  style?: StyleProp<ViewStyle>;
+};
 
-const Toast: React.FC<React.PropsWithChildren<Props>> = ({}) => {
+const Toast: React.FC<React.PropsWithChildren<Props>> = ({
+  visible = false,
+  style,
+  labelStyle,
+}) => {
+  if (!visible) return null;
+  const containerStyle = StyleSheet.flatten([
+    extractBorderAndMarginStyles(style).marginStyles,
+  ]);
   return (
-    <View>
-      <Text>Test</Text>
+    <View style={containerStyle}>
+      <Text style={[labelStyle]}>Test</Text>
     </View>
   );
 };

e1957357b622c91d142ccf83c3dd7d586c5ddbc8
sieu-db <sieu@draftbit.com>
2024-06-30 23:09:54 +0700

    feat: add dump test toast component

---
+++ packages/core/src/components/Toast/Toast.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+type Props = {};
+
+const Toast: React.FC<React.PropsWithChildren<Props>> = ({}) => {
+  return (
+    <View>
+      <Text>Test</Text>
+    </View>
+  );
+};
+
+export default Toast;

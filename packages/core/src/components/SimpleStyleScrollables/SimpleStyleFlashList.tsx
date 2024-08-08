c25d963f753d24c860982618ed7e41503c7c62ab
YoussefHenna <youssef.hisham14@gmail.com>
2024-08-08 13:26:44 +0300

    Only include styles that flash list supports

--- packages/core/src/components/SimpleStyleScrollables/SimpleStyleFlashList.tsx
+++ packages/core/src/components/SimpleStyleScrollables/SimpleStyleFlashList.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { FlashList } from "@shopify/flash-list";
 import type { FlashListProps, ContentStyle } from "@shopify/flash-list";
 import useSplitContentContainerStyles from "./useSplitContentContainerStyles";
+import { pick } from "lodash";
 
 /**
  * A FlashList wrapper that takes a single `style` prop and internally extracts
@@ -19,11 +20,24 @@ const SimpleStyleFlashList = React.forwardRef(
     const { style, contentContainerStyle } =
       useSplitContentContainerStyles(styleProp);
 
+    // FlashList only supports a subset of contentContainerStyles
+    // See https://shopify.github.io/flash-list/docs/usage/#contentcontainerstyle
+    const flashListContentContainerStyle = pick(contentContainerStyle, [
+      "backgroundColor",
+      "paddingTop",
+      "paddingLeft",
+      "paddingRight",
+      "paddingBottom",
+      "padding",
+      "paddingVertical",
+      "paddingHorizontal",
+    ]);
+
     return (
       <FlashList
         ref={ref}
         style={style}
-        contentContainerStyle={contentContainerStyle as ContentStyle}
+        contentContainerStyle={flashListContentContainerStyle as ContentStyle}
         data={data}
         {...rest}
       />

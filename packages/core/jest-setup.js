4615627581827e8372abd9753c1dd22b29387a3d
YoussefHenna <youssef.hisham14@gmail.com>
2024-08-25 14:46:30 +0300

    Fix tests

--- packages/core/jest-setup.js
+++ packages/core/jest-setup.js
@@ -6,3 +6,7 @@ setupReaanimatedTests();
 global.ReanimatedDataMock = {
   now: () => 0,
 };
+
+jest.mock("@react-native-async-storage/async-storage", () =>
+  require("@react-native-async-storage/async-storage/jest/async-storage-mock")
+);
--- packages/maps/jest-setup.js
+++ packages/maps/jest-setup.js
@@ -3,3 +3,7 @@ jest.mock("react-native/Libraries/EventEmitter/NativeEventEmitter");
 jest.mock("react-native-webview", () => ({
   default: () => jest.fn(), // or any mocked component instead of native view,
 }));
+
+jest.mock("@react-native-async-storage/async-storage", () =>
+  require("@react-native-async-storage/async-storage/jest/async-storage-mock")
+);

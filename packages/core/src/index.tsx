ad12d8cf8bd977c72ae80b58fe02a47fa39d1e9a
Youssef Henna <58384527+YoussefHenna@users.noreply.github.com>
2024-10-23 12:03:03 +0200

    Add `swipeCard` method to DeckSwiper components (#959)
    * Add `swipeCard` method to DeckSwiper components
    
    * Add default case for swiping

--- packages/core/src/index.tsx
+++ packages/core/src/index.tsx
@@ -34,6 +34,7 @@ export {
 } from "./components/RadioButton/index";
 export { default as Shadow } from "./components/Shadow";
 export { DeckSwiper, DeckSwiperCard } from "./components/DeckSwiper";
+export type { DeckSwiperRef } from "./components/DeckSwiper";
 export { TabView, TabViewItem } from "./components/TabView";
 export { default as Markdown } from "./components/Markdown";
 export { BottomSheet } from "./components/BottomSheet";

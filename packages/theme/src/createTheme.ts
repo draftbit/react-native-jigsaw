7f79d7423010534443102813a37e66a9e7d04c9b
Youssef Henna <youssef.hisham14@gmail.com>
2024-11-19 19:06:50 +0100

    remove deepmerge-ts

--- packages/theme/src/createTheme.ts
+++ packages/theme/src/createTheme.ts
@@ -1,4 +1,3 @@
-import { deepmergeCustom } from "deepmerge-ts";
 import type {
   Breakpoints,
   Theme,
@@ -6,11 +5,11 @@ import type {
   ColorPalettes,
 } from "./types";
 import {
-  isTextStyleObject,
   validateBreakpoints,
   validatePalettes,
   validateTheme,
 } from "./validators";
+import merge from "deepmerge";
 
 /**
  * Custom deepmerge function to skip merging of typography/text style objects.
@@ -24,20 +23,6 @@ import {
  * keys and the keys of the style object which breaks how the proxy is able to
  * return the correct value.
  */
-const themeMerge = deepmergeCustom({
-  enableImplicitDefaultMerging: true,
-  mergeRecords(values, utils, meta) {
-    const firstValue = values[0];
-    if (isTextStyleObject(firstValue)) {
-      return utils.defaultMergeFunctions.mergeRecords(
-        values.slice(1),
-        utils,
-        meta
-      );
-    }
-    return utils.defaultMergeFunctions.mergeRecords(values, utils, meta);
-  },
-});
 
 export default function createTheme({
   breakpoints,
@@ -58,7 +43,7 @@ export default function createTheme({
 
   if (baseTheme) {
     validateTheme(baseTheme);
-    resultTheme = themeMerge(baseTheme, theme);
+    resultTheme = merge(baseTheme, theme);
   }
 
   return {

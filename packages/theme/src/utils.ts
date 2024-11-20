dcdc06e7ce710a03da3c7a21479d0c7a9add6b02
Youssef Henna <youssef.hisham14@gmail.com>
2024-11-20 15:02:41 +0100

    Handle value not an object in `isTextStyleObject`

--- packages/theme/src/utils.ts
+++ packages/theme/src/utils.ts
@@ -2,16 +2,22 @@ import { isObject } from "lodash";
 import type { ThemeValues } from "./types";
 
 export function isTextStyleObject(value: any): boolean {
-  return Object.keys(value).some((key) =>
-    [
-      "fontFamily",
-      "fontWeight",
-      "fontSize",
-      "fontStyle",
-      "lineHeight",
-      "letterSpacing",
-    ].includes(key)
-  );
+  try {
+    return Object.keys(value).some((key) =>
+      [
+        "fontFamily",
+        "fontWeight",
+        "fontSize",
+        "fontStyle",
+        "lineHeight",
+        "letterSpacing",
+      ].includes(key)
+    );
+  } catch (e) {
+    // If `value` is not an object, the above code will throw an error
+    // Catch error and return false is more efficient than a call to `isObject`
+    return false;
+  }
 }
 
 export function asThemeValuesObject(value: any): ThemeValues | null {

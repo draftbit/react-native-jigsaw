f5c87060cb239ccb77c6cb57854e2ee932b8fba2
Youssef Henna <youssef.hisham14@gmail.com>
2024-11-20 12:35:21 +0100

    Revert "clear up useEffect"
    This reverts commit f5eddec44c541b9a11e8714b470647cb469c47d4.

--- packages/theme/src/Provider.tsx
+++ packages/theme/src/Provider.tsx
@@ -107,7 +107,15 @@ const Provider: React.FC<React.PropsWithChildren<ProviderProps>> = ({
         SAVED_SELECTED_THEME_KEY
       );
       if (savedSelectedThemeName) {
-        changeTheme(savedSelectedThemeName);
+        const themeExists = themes.some(
+          (t) => t.name === savedSelectedThemeName
+        );
+        console.log("RUNNING", savedSelectedThemeName);
+
+        if (themeExists) {
+          changeTheme(savedSelectedThemeName);
+        } else {
+        }
       }
     };
     run();

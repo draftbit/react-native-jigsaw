07571fd46e22017e5226211b4ba25cb78b634502
sieu-db <159281348+sieu-db@users.noreply.github.com>
2025-01-13 23:50:49 +0700

    Change to using Flatlist from gesture-handler - 52 (#984)
    * update to using Flatlist from gesture-handler
    
    * fix typescript

--- packages/core/src/components/FlatList.tsx
+++ packages/core/src/components/FlatList.tsx
@@ -1,14 +1,14 @@
 import React from "react";
-import { FlatList as FlatListstComponent } from "react-native";
+import { FlatList as FlatListComponent } from "react-native-gesture-handler";
 import type { FlatListProps } from "react-native";
 
-const FlatList = React.forwardRef<FlatListstComponent, FlatListProps<any>>(
+const FlatList = React.forwardRef<FlatListComponent, FlatListProps<any>>(
   <T extends any>(
     { numColumns, ...rest }: FlatListProps<T>,
-    ref: React.Ref<FlatListstComponent>
+    ref: React.Ref<FlatListComponent>
   ) => {
     return (
-      <FlatListstComponent
+      <FlatListComponent
         key={numColumns} // Changing numColumns requires re-rendering, setting it as the key ensures list is re-rendered when it changes
         numColumns={numColumns}
         ref={ref}
